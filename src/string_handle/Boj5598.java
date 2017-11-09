0aaffa924c5bbd46ad8d31fa1187e0fc8e2e7a25
minuk8932 <minuk8932@naver.com>
2017-11-09 21:49:07 +0900

    add StringHandle

---
+++ src/string_handle/Boj5598.java
@@ -0,0 +1,24 @@
+package string_handle;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj5598 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line = br.readLine();
+		char[] str = line.toCharArray();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < str.length; i++){
+			if(str[i] > 'C'){
+				str[i] = (char) (str[i] - 3);
+			}
+			else{
+				str[i] = (char)(str[i] + 23);
+			}
+			sb.append(str[i]);
+		}
+		System.out.println(sb.toString());
+	}
+}
