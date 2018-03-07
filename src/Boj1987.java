823ec9ee504e6d580f4b39086c81b5ac92d9832e
minuk8932 <minuk8932@naver.com>
2018-03-07 20:30:18 +0900

    add Dijkstra(Particular Minimum Path) 180307

---
+++ src/Boj1987.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj1987 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
