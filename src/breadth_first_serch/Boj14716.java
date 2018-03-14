27220ad7fd4b80ba014f292fe40c186b75a863d9
minuk8932 <minuk8932@naver.com>
2018-03-14 22:05:27 +0900

    add comment 14716

--- src/breadth_first_serch/Boj14716.java
+++ src/breadth_first_serch/Boj14716.java
@@ -5,6 +5,14 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * 	@author minchoba
+ *	백준 14716번 : 현수막
+ *
+ *	@see https://www.acmicpc.net/problem/14716
+ *
+ */
 public class Boj14716 {
 	private static final String SPACE = " ";
 
@@ -14,6 +22,7 @@ public class Boj14716 {
 	private static final int COL = 1;
 
 	public static void main(String[] args) throws Exception {
+		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
 
@@ -31,20 +40,20 @@ public class Boj14716 {
 			}
 		}
 		
-		for (int row = 0; row < M; row++) {
+		for (int row = 0; row < M; row++) {										// 해당 인덱스의 값이 1인 경우를 찾는다.
 			for (int col = 0; col < N; col++) {
 				if (card[row][col] == 1 && isVisited[row][col] == 0) {
 					isVisited[row][col] = 1;
 					
 					cnt++;
 					
-					Queue<Point> queue = new LinkedList<>();
+					Queue<Point> queue = new LinkedList<>();				// BFS로 탐색
 					queue.offer(new Point(row, col));
 
 					while (!queue.isEmpty()) {
 						Point current = queue.poll();
 
-						for (final int[] DIRECTION : DIRECTIONS) {
+						for (final int[] DIRECTION : DIRECTIONS) {				// 기본 BFS와 같은 4방향이아닌 대각선도 체크해야하므로, 총 8방향을 넣어가며 다음 인덱스를 찾는다.
 							int nextRow = DIRECTION[ROW] + current.row;
 							int nextCol = DIRECTION[COL] + current.col;
 
@@ -64,9 +73,15 @@ public class Boj14716 {
 			}
 		}
 
-		System.out.println(cnt);
+		System.out.println(cnt);				// 결과값 출력
 	}
-
+	
+	/**
+	 * 
+	 * 	@author minchoba
+	 *	정점 이너 클래스
+	 *
+	 */
 	private static class Point {
 		int row = 0;
 		int col = 0;
