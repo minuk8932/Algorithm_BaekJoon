82503f7d8d0734c7f932c36350fd6541867c135e
minuk8932 <minuk8932@naver.com>
2018-03-14 22:38:13 +0900

    add comment 2667

--- src/breadth_first_serch/Boj2667.java
+++ src/breadth_first_serch/Boj2667.java
@@ -7,15 +7,14 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/*
- * 	약식
- * 	1 1 0 0
- * 	0 0 0 1			=> 열마다 갈 수 있는지? 방문했는지 일일이 검사해보면서 돌아간다.
- * 	1 1 0 0
- * 	1 1 0 1
+/**
  * 
+ * 	@author minchoba
+ *	백준 2667번 : 단지번호 붙이기
+ *
+ *	@see https://www.acmicpc.net/problem/2667
+ *
  */
-
 public class Boj2667 {
 	private static final int[][] DIRECTIONS = {{1, 0}, {-1, 0}, {0, 1}, {0,-1}};
 	private static final int ROW = 0;
@@ -24,6 +23,7 @@ public class Boj2667 {
 	public static final String NEW_LINE = "\n";
 	
 	public static void main(String[] args) throws Exception {
+		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
@@ -42,9 +42,9 @@ public class Boj2667 {
 		boolean[][] isVisited = new boolean[N][N];
 		
 		int areaCnt = 0;
-		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>();				// 가구수를 담을 리스트
 		
-		for(int row = 0; row < N; row++){			
+		for(int row = 0; row < N; row++){							// BFS 실행, 가구가 존재하면서 방문하지 않은 곳
 			for(int col = 0; col < N; col++){
 				if(map[row][col] == 1 && !isVisited[row][col]){
 					isVisited[row][col] = true;
@@ -52,7 +52,7 @@ public class Boj2667 {
 					Queue<Point> queue = new LinkedList<>();	
 					queue.offer(new Point(row, col));
 					
-					areaCnt++;
+					areaCnt++;								// 단지 수 증가
 					int nodeCnt = 1;
 					
 					while(!queue.isEmpty()){
@@ -65,19 +65,19 @@ public class Boj2667 {
 							if(0 <= nextRow && nextRow < N && 0 <= nextCol && nextCol < N){
 								if(map[nextRow][nextCol] == 1 && !isVisited[nextRow][nextCol]){
 									isVisited[nextRow][nextCol] = true;
-									nodeCnt++; // 탐색 할때마다 노드 수 증가 시킨다, 가구의 수 체크 
+									nodeCnt++; 													// 가구의 수 증가 
 									queue.offer(new Point(nextRow, nextCol));
 								}
 							}
 						}
 					}
-					list.add(nodeCnt);
+					list.add(nodeCnt);					// 가구 수 저장
 				}
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		sb.append(areaCnt).append(NEW_LINE);
+		sb.append(areaCnt).append(NEW_LINE);		// 각 결과들을 정리해 버퍼에 담은 후
 		
 		Collections.sort(list);
 		
@@ -85,9 +85,15 @@ public class Boj2667 {
 			sb.append(nodeCnt).append(NEW_LINE);
 		}
 		
-		System.out.println(sb.toString());
+		System.out.println(sb.toString());			// 결과값 한번에 출력
 	}
 	
+	/**
+	 * 
+	 * 	@author minchoba
+	 *	정점 이너 클래스
+	 *
+	 */
 	private static class Point{
 		public int row;
 		public int col;
