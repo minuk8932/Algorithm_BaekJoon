bb7d358333acbc3a1e2aa6fd05bd66ead539dd98
minuk8932 <minuk8932@naver.com>
2018-03-23 21:48:35 +0900

    add comment

--- src/Boj2790.java
+++ src/Boj2790.java
@@ -3,34 +3,46 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Boj2790 {
+	private static final int INF = 300_001;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		int[] race = new int[N + 1];
-		for(int i = 1; i < N + 1; i++){
+		int[] race = new int[N];
+		int[] first = new int[N];
+		int[] second = new int[N];
+		
+		boolean[] isWinner = new boolean[INF];
+		
+		for(int i = 0; i < N; i++){
 			race[i] = Integer.parseInt(br.readLine());
 		}
 		
-		Arrays.sort(race);
-		int max = 0, cnt = 0;
-		
-		for(int i = 1; i < N+1; i++){
-			System.out.println(race[i]);
+		for(int i = 0; i < N; i++){
+			first[i] = N + race[i];
+			second[i] = (N - 1) + race[i];
 		}
 		
-		for(int i = 1; i < N + 1; i++){
-			race[i] += (N + 1) - i;
-			if(race[i] > max){
-				max = Math.max(race[i], max);
+		for(int i = 0; i < N; i++){
+			for(int j = i + 1; j < N; j++){
+				if(first[i] > second[j]){
+					isWinner[first[i]] = false;
+				}
+				else{
+					isWinner[first[i]] = true;
+				}
 			}
 		}
 		
-		for(int i = 1; i < N + 1; i++){
-			if(max == race[i]){
-				cnt++;
+		int res = 0;
+		
+		for(int i = 1; i < isWinner.length; i++){
+			if(isWinner[i]){
+				res++;
 			}
 		}
-		System.out.println(cnt);
+		
+		
+		System.out.println(res);
 	}
 }
--- src/dynamic_programming/Boj1003.java
+++ src/dynamic_programming/Boj1003.java
@@ -3,11 +3,20 @@ package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 
+ * 	@author minchoba
+ *	백준 1003번 : 피보나치 함수
+ *
+ *	@see https://www.acmicpc.net/problem/1003/
+ *
+ */
 public class Boj1003 {
 	private static final String SPACE = " ";
 	private static final String NEW_LINE = "\n";
 
 	public static void main(String[] args) throws Exception {
+		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine());
@@ -15,18 +24,18 @@ public class Boj1003 {
 		StringBuilder sb = new StringBuilder();
 		while (T-- > 0) {
 			int num = Integer.parseInt(br.readLine());
-			int[][] chk = new int[2][41];
+			int[][] chk = new int[2][41];							// idx0 : 0의 출력횟수, idx1 : 1의 출력횟수
 			
 			chk[0][0] = 1;
 			chk[1][1] = 1;
 			
-			for(int i = 2; i < 41; i++){
+			for(int i = 2; i < 41; i++){								// 피보나치 함수 실행
 				chk[0][i] = chk[0][i - 2] + chk[0][i - 1];
 				chk[1][i] = chk[1][i - 1] + chk[1][i - 2];
 			}
 
-			sb.append(chk[0][num]).append(SPACE).append(chk[1][num]).append(NEW_LINE);
+			sb.append(chk[0][num]).append(SPACE).append(chk[1][num]).append(NEW_LINE);	//0 과 1이 출력되는 횟수를 버퍼에 담음
 		}
-		System.out.println(sb.toString());
+		System.out.println(sb.toString());		// 결과값 한번에 출력
 	}
 }
