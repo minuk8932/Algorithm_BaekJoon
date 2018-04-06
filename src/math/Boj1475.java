40840301c049124519faa1c8645a10b98ac55fd8
minuk8932 <minuk8932@naver.com>
2018-04-06 11:35:29 +0900

    add comment and modify code

--- src/math/Boj1475.java
+++ src/math/Boj1475.java
@@ -1,59 +1,49 @@
 package math;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 
+ * 	@author minchoba
+ *	백준 1475번 : 방번호
+ *
+ *	@see https://www.acmicpc.net/problem/1475/
+ *
+ */
 public class Boj1475 {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
-		int len = line.length();
-		
-		int[] num = new int[len];
-		
-		for(int i = 0; i < len; i++){	
-			num[i] = Character.getNumericValue(line.charAt(i));
-		}
-		
-		boolean[] nums = new boolean[10];
-		int  cnt = 0;
-		
-		for(int i = 0; i < num.length; i++){
-			switch(num[i]){
-			case '0':
-				if(!nums[num[i]]){
-					nums[num[i]] = true;
-				}
-				break;
-				
-			case '1':
-				break;
-			
-			case '2':
-				break;
-			
-			case '3':
-				break;
-			
-			case '4':
-				break;
-			
-			case '5':
-				break;
-			
-			case '6':
-				break;
-			
-			case '7':
-				break;
-			
-			case '8':
-				break;
-			
-			case '9':
-				break;
 
+		String str = br.readLine();
+		char[] N = str.toCharArray();
+
+		int[] num = new int[N.length];
+
+		for (int i = 0; i < N.length; i++) {
+			num[i] = Character.getNumericValue(N[i]);
+		}
+
+		int[] chk = new int[10];
+		int cnt = 0;
+		int max = 0;
+
+		for (int i = 0; i < N.length; i++) {			// 0~9사이의 값만 받아서, 확인 배열에 더해줌
+			chk[num[i]]++;
+		}
+
+		cnt = chk[6] + chk[9];							// 이중 6, 9는 뒤집어 쓸 수 있으므로, 그러한 가능성을 추가
+		cnt = (cnt / 2) + (cnt % 2);
+
+		for (int i = 0; i < chk.length; i++) {		// 6, 9가 아닐 경우 결과를 더하고
+			if (i != 6 && i != 9) {
+				if (chk[i] >= max) {
+					max = chk[i];
+				}
 			}
 		}
-	}
 
+		System.out.println(Math.max(max, cnt));		// 두 값 중 최댓값 출력
+
+	}
 }
