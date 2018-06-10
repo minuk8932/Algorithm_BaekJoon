f3fd008ca76de9eee848327b081c1db9cc03c560
minuk8932 <minuk8932@naver.com>
2018-06-10 19:27:18 +0900

    add Comment(Quadrant) 180610

--- src/math/Boj9610.java
+++ src/math/Boj9610.java
@@ -3,11 +3,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * 	@author minchoba
+ *	백준 9610번: 사분면
+ *
+ *	@see https://www.acmicpc.net/problem/9610/
+ *
+ */
 public class Boj9610 {
 	private static final String[] PRINT = {"Q1: ", "Q2: ", "Q3: ", "Q4: ", "AXIS: ",};
 	private static final String NEW_LINE = "\n";
 	
 	public static void main(String[] args) throws Exception{
+		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int[] axis = new int[5];
@@ -17,7 +26,7 @@ public class Boj9610 {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			if(x == 0 || y== 0) axis[4]++;
+			if(x == 0 || y== 0) axis[4]++;		// 사분면의 정의에따라 값을 더해줌
 			else if(x > 0 && y > 0) axis[0]++;
 			else if(x < 0 && y > 0) axis[1]++;
 			else if(x < 0 && y < 0) axis[2]++;
@@ -25,10 +34,10 @@ public class Boj9610 {
 		}
 		
 		StringBuilder sb = new StringBuilder();		
-		for(int i = 0; i < PRINT.length; i++) {
+		for(int i = 0; i < PRINT.length; i++) {					// 각 결과를 버퍼에 담은 후
 			sb.append(PRINT[i]).append(axis[i]).append(NEW_LINE);
 		}
 		
-		System.out.println(sb.toString());
+		System.out.println(sb.toString());		// 결과값 한번에 출력
 	}
 }
