7099984045578acd18f364f6797c550cb1e2b4d9
minuk8932 <minuk8932@naver.com>
2018-08-06 22:49:36 +0900

    edit Contest(BSIS F) 180806

--- src/baekjoon_contest/BSIS_ProblemG.java
+++ src/baekjoon_contest/BSIS_ProblemG.java
@@ -7,39 +7,68 @@ import java.util.StringTokenizer;
 public class BSIS_ProblemG {
 	private static final String NEW_LINE = "\n";
 	
+	private static final int INF = 500_001;
+	
+	private static Water[] w = new Water[INF];
+	private static Range[] r = new Range[INF];
+	
+	private static long[] values = new long[INF];
+	private static int[] seq = new int[INF];
+	
+	private static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int Q = Integer.parseInt(st.nextToken());
 		
-		Water[] w = new Water[N + 1];
-		w[0] = new Water(0, 0);
 		st = new StringTokenizer(br.readLine());
 		
 		for(int i = 1; i < N + 1; i++) {
-			w[i] = new Water(-i, Integer.parseInt(st.nextToken()));
+			w[i] = new Water(-i, Long.parseLong(st.nextToken()) + 1L);
 		}
 		
-		StringBuilder sb = new StringBuilder();
+//		ArrayList<Range> r = new ArrayList<>();
 		
-		Range[] r = new Range[Q + 1];
-		int[] values = new int[N + 1];
+		r[0] = new Range(-1, -1);
 		
-		for(int i = 0; i < Q; i++) {
+		for(int i = 1; i < Q + 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			int T = Integer.parseInt(st.nextToken());
-			int R = Integer.parseInt(st.nextToken());
 			int L = Integer.parseInt(st.nextToken());
+			int R = Integer.parseInt(st.nextToken());
 
-			r[T] = new Range(R, L);
+			r[T] = new Range(L, R);
+//			r.add(new Range(L, R));
+			seq[i] = T;
+		}
+		
+		w[0] = new Water(0, 0);
+		
+		for(int i = 1; i < Q + 1; i++) {
+			long cnt = 0;
+			w[0].pos++;
+			
+			for(int j = 1; j < N + 1; j++) {
+				if(Math.abs(w[j - 1].pos - w[j].pos) >= w[j].dist) w[j].pos = w[j - 1].pos - 1;
+			}
+			
+//			Range ran = r.remove(0);
+			
+			for(int j = 0; j < N + 1; j++) {				
+				if(w[j].pos >= r[i].left && w[j].pos <= r[i].right) cnt++;
+			}
+			
+			values[i] = cnt;
+//			sb.append(cnt).append(NEW_LINE);
 		}
 		
 		for(int i = 1; i < Q + 1; i++) {
-			
+			sb.append(values[seq[i]]).append(NEW_LINE);
 		}
 		
-		
+		System.out.println(sb.toString());
 	}
 	
 	private static class Range{
@@ -54,11 +83,11 @@ public class BSIS_ProblemG {
 	
 	private static class Water{
 		int pos;
-		int move;
+		long dist;
 		
-		public Water(int pos, int move) {
+		public Water(int pos, long dist) {
 			this.pos = pos;
-			this.move = move;
+			this.dist = dist;
 		}
 	}
 }
