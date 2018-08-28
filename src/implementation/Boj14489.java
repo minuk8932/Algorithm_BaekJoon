3c1927f883a702a6e9a9cd5563ee04b80ee478dd
minuk8932 <minuk8932@naver.com>
2018-08-28 19:33:17 +0900

    add Implementation(Two Chickens ...) 180828

--- src/implementation/Boj14489.java
+++ src/implementation/Boj14489.java
@@ -17,8 +17,8 @@ public class Boj14489 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int A = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());	// 욱제의 통장 잔고
-		int C = Integer.parseInt(br.readLine());					// 치킨 한마리 가격
+		int C = Integer.parseInt(br.readLine()) * 2;					// 치킨 한마리 가격
 		
-		System.out.println(A >= C * 2 ? A - C * 2 : A);	// 조건에 따라 결과 값 출력
+		System.out.println(A >= C ? A - C : A);	// 조건에 따라 결과 값 출력
 	}
 }
