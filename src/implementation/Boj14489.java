03e6d27850d19cfa5235dea8c318b5f4d502adb7
minuk8932 <minuk8932@naver.com>
2018-08-28 19:32:06 +0900

    add Implementation(Two Chickens ...) 180828

---
+++ src/implementation/Boj14489.java
@@ -0,0 +1,24 @@
+package implementation;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 	@author minchoba
+ *	백준 14489번: 치킨 두 마리(..)
+ *
+ *	@see https://www.acmicpc.net/problem/14489/
+ *
+ */
+public class Boj14489 {
+	public static void main(String[] args) throws Exception {
+		// 버퍼를 통한 값 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());	// 욱제의 통장 잔고
+		int C = Integer.parseInt(br.readLine());					// 치킨 한마리 가격
+		
+		System.out.println(A >= C * 2 ? A - C * 2 : A);	// 조건에 따라 결과 값 출력
+	}
+}
