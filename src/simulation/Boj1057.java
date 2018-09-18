51627d61a4e94dd2e2270378de9b8dbc6c6db803
minuk8932 <minuk8932@naver.com>
2018-09-18 16:16:29 +0900

    add Simulation(Tournament) 180918

---
+++ src/simulation/Boj1057.java
@@ -0,0 +1,42 @@
+package simulation;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 	@author minchoba
+ *	백준 1057번: 토너먼트
+ *
+ *	@see https://www.acmicpc.net/problem/1057/
+ *
+ */
+public class Boj1057 {
+	public static void main(String[] args) throws Exception{
+		// 버퍼를 통한 값 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int r1 = Integer.parseInt(st.nextToken());
+		int r2 = Integer.parseInt(st.nextToken());
+		
+		r1 = r1 % 2 == 0 ? r1 : r1 + 1;		// 시작하는 라운드의 값을 짝수로 변경, 다음 라운드 부전승 신경을 쓸 필요 없음
+		r2 = r2 % 2 == 0 ? r2 : r2 + 1;
+		
+		int rCnt = 1;
+		
+		while(N-- > 0) {
+			if(r1 == r2) break;		// 두 값이 같아지는 경우 라운드에서 만남
+			
+			r1 /= 2;			// 현 라운드 값을 2로 나누면 다음 라운드에서의 순번이 정해짐
+			r2 /= 2;
+			
+			r1 = r1 % 2 == 0 ? r1 : r1 + 1;		// 라운드마다 홀수가 생기면 짝수로 변경	
+			r2 = r2 % 2 == 0 ? r2 : r2 + 1;
+			
+			rCnt++;		// 라운드 + 1
+		}
+		
+		System.out.println(rCnt); // 최종 만나는 라운드를 출력
+	}
+}
