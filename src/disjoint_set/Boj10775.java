c279dc72687104850786d677ade09e41ab4343fd
minuk8932 <minuk8932@naver.com>
2018-10-04 14:27:26 +0900

    add Disjoint Set(Air Port) 181004

--- src/disjoint_set/Boj10775.java
+++ src/disjoint_set/Boj10775.java
@@ -1,3 +1,4 @@
+package disjoint_set;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -26,23 +27,31 @@ public class Boj10775 {
 
 		for (int i = 1; i < P + 1; i++) {
 			int gate = Integer.parseInt(br.readLine());
-			int docked = find(gate);
+			int docked = find(gate);		// 각 연결된 게이트를 찾고
 
-			if (docked == 0) break;
-			merge(docked, docked - 1);
-			++res;
+			if (docked == 0) break;			// 도킹이 불가능하면 반복문종료
+			merge(docked, docked - 1);		// 도킹이 가능하고, 현재 게이트가 도킹이 된 상태면 해당 게이트 - 1번째 게이트와 union 후
+			++res;						// 도킹한 비행기 수 + 1
 		}
 
-		System.out.println(res);
+		System.out.println(res);		// 도킹한 비행기 수 출력
 	}
 
+	/**
+	 * union 메소드
+	 * 
+	 */
 	private static void merge(int x, int y) {
 		x = find(x);
 		y = find(y);
 
 		plane[x] = y;
 	}
-
+	
+	/**
+	 * find 메소드
+	 * 
+	 */
 	private static int find(int air) {
 		if (plane[air] == air) {
 			return air;
