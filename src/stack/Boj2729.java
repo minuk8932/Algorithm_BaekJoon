2ac4ad7cf8f8770c81a7097e9386f7a8cf29261e
minuk8932 <minuk8932@naver.com>
2018-11-14 16:41:25 +0900

    refactor Stack(Binary Addition) 181114

--- src/stack/Boj2729.java
+++ src/stack/Boj2729.java
@@ -14,6 +14,11 @@ import java.util.StringTokenizer;
  */
 public class Boj2729 {
 	private static final String NEW_LINE = "\n";
+	private static final char ZERO = '0';
+	private static final char ONE = '1';
+	
+	private static Stack<Character> lifo;
+	private static int counts = 0;
 	
 	public static void main(String[] args) throws Exception{
 		// 버퍼를 통한 값 입력
@@ -22,89 +27,91 @@ public class Boj2729 {
 		int T = Integer.parseInt(br.readLine());
 		
 		while(T-- > 0) {
+			lifo = new Stack<>();
+			counts = 0;
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String b1 = st.nextToken();
 			String b2 = st.nextToken();
 			
 			int leng1 = b1.length();
 			int leng2 = b2.length();
-			
 			int leng = Math.max(leng1, leng2);
-			char[] bi1 = new char[leng];
-			char[] bi2 = new char[leng];
+			
+			char[] bin1 = new char[leng];
+			char[] bin2 = new char[leng];
 			
 			if(leng1 > leng2) {							// 길이가 안맞는 이진수의 길이를 맞춰줌
-				for(int i = 0; i < leng; i++) {
-					bi1[i] = b1.charAt(i);
-					
-					if(i < leng - leng2) bi2[i] = '0';
-					else bi2[i] = b2.charAt(i - (leng - leng2));
-				}
+				setArray(leng, b1, b2, bin1, bin2, leng1, leng2);
 			}
 			else {
-				for(int i = 0; i < leng; i++) {
-					bi2[i] = b2.charAt(i);
-					
-					if(i < leng - leng1) bi1[i] = '0';
-					else bi1[i] = b1.charAt(i - (leng - leng1));
-				}
+				setArray(leng, b2, b1, bin2, bin1, leng2, leng1);
 			}
-			
-			Stack<Character> lifo = new Stack<>();
-			int carry = 0, counts = 0;
-			
-			for(int i = leng - 1; i >= 0; i--) {		// 조건에 따라 스택에 들어갈 값을 결정
-				if(bi1[i] == '0' && bi2[i] == '0') {
-					counts++;
-					
-					if(carry == 0) {
-						lifo.push('0');
-					}
-					else {
-						lifo.push('1');
-						carry = 0;
-					}
-				}
-				else if((bi1[i] == '0' && bi2[i] == '1') || (bi1[i] == '1' && bi2[i] == '0')) {
-					if(carry == 0) {
-						lifo.push('1');
-					}
-					else {
-						lifo.push('0');
-						carry = 1;
-					}
-				}
-				else {
-					if(carry == 0) {
-						lifo.push('0');
-						carry = 1;
-					}
-					else {
-						lifo.push('1');
-						carry = 1;
-					}
-				}
-			}
-			
-			if(carry == 1) lifo.push('1');			// 마지막 연산 중 캐리가 발생 할 경우
-			
-			if(counts == leng) {				// 모든 값이 0인 경우
-				sb.append(0);
-			}
-			else {
-				boolean isOne = false;
-				
-				while(!lifo.isEmpty()) {
-					if(lifo.peek() == '1') isOne = true; 
-					
-					if(isOne) sb.append(lifo.pop());		// 1이 나오기전까진 앞자린 필요없으므로 다 버림
-					else lifo.pop();
-				}
-			}
-			
-			sb.append(NEW_LINE);
+
+			sb.append(output(leng));
 		}
 		
 		System.out.println(sb);			// 결과 값 한번에 출력 
+	}
+	
+	private static void setArray(int leng, String oneBin, String anotherBin, char[] arr1, char[] arr2, int leng1, int leng2) {		
+		for(int i = 0; i < leng; i++) {
+			arr1[i] = oneBin.charAt(i);
+			
+			if(i < leng - leng2) arr2[i] = ZERO;
+			else arr2[i] = anotherBin.charAt(i - (leng - leng2));
+		}
+		
+		stackConsist(leng, arr1, arr2);
+	}
+	
+	private static void stackConsist(int leng, char[] arr1, char[] arr2) {
+		int carry = 0;
+		
+		for(int i = leng - 1; i >= 0; i--) {		// 조건에 따라 스택에 들어갈 값을 결정
+			int sum = (arr1[i] - ZERO) + (arr2[i] - ZERO);
+			carry = options(carry, sum);
+		}
+		
+		if(carry == 1) lifo.push(ONE);			// 마지막 연산 중 캐리가 발생 할 경우
+	}
+	
+	private static int options(int carry, int opt) {
+		if(opt == 0) counts++;
+		
+		if(carry == 0) {
+			lifo.push((char) ((opt % 2) + ZERO));
+			
+			if(opt == 2) carry = 1;
+		}
+		else {
+			lifo.push((char) (((opt + 1) % 2) + ZERO));
+			
+			if(opt == 0) carry = 0;
+			else carry = 1;
+		}
+		
+		return carry;
+	}
+	
+	private static String output(int leng) {
+		StringBuilder tmp = new StringBuilder();
+		
+		if(counts == leng) {				// 모든 값이 0인 경우
+			tmp.append(0);
+		}
+		else {
+			boolean isOne = false;
+			
+			while(!lifo.isEmpty()) {
+				if(lifo.peek() == ONE) isOne = true; 
+				
+				if(isOne) tmp.append(lifo.pop());		// 1이 나오기전까진 앞자린 필요없으므로 다 버림
+				else lifo.pop();
+			}
+		}
+
+		tmp.append(NEW_LINE);
+		return tmp.toString();
 	}
 }
