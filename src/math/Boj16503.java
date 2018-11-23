171326032e38c3183a77d9f4839f02a95f2d4997
minuk8932 <minuk8932@naver.com>
2018-11-23 17:04:09 +0900

    refactoring Math(Non Parenthesis Operation) 181123

--- src/math/Boj16503.java
+++ src/math/Boj16503.java
@@ -13,77 +13,37 @@ import java.util.StringTokenizer;
  */
 public class Boj16503 {
 	private static final String NEW_LINE = "\n";
-	private static final String MUL = "*";
-	private static final String DIV = "/";
-	private static final String ADD = "+";
-	private static final String SUB = "-";
-	
-	private static boolean[][] isUsed = new boolean[2][4];
+	private static final char MUL = '*';
+	private static final char DIV = '/';
+	private static final char ADD = '+';
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k1 = Integer.parseInt(st.nextToken());
+		char o1 = st.nextToken().charAt(0);
+		int k2 = Integer.parseInt(st.nextToken());
+		char o2 = st.nextToken().charAt(0);
+		int k3 = Integer.parseInt(st.nextToken());
 		
-		System.out.println(getMinMax(Integer.parseInt(st.nextToken()), st.nextToken(), 
-				Integer.parseInt(st.nextToken()), st.nextToken(), Integer.parseInt(st.nextToken())));
+		System.out.println(getMinMax(k1, o1, k2, o2, k3));
 	}
 	
-	private static StringBuilder getMinMax(int K1, String O1, int K2, String O2, int K3) {
+	private static StringBuilder getMinMax(int K1, char O1, int K2, char O2, int K3) {
 		StringBuilder sb = new StringBuilder();
-		int sum1 = calculator(O1, K1, K2, 0);
-		int sum2 = calculator(O2, K2, K3, 1);
+		int sum1 = calculator(calculator(K1, O1, K2), O2, K3);
+		int sum2 = calculator(K1, O1, calculator(K2, O2, K3));
 		
-		sum1 = nextCalculate(sum1, K3, 1);
-		sum2 = nextCalculate(K1, sum2, 0);
-		
-		sb.append(sum1 < sum2 ? sum1 : sum2).append(NEW_LINE).append(sum1 > sum2 ? sum1 : sum2);		
+		if(sum1 > sum2) sb.append(sum2).append(NEW_LINE).append(sum1);
+		else sb.append(sum1).append(NEW_LINE).append(sum2);	
+				
 		return sb;
 	}
 	
-	private static int calculator(String oper, int num1, int num2, int idx) {
-		int value = 0;
-		
-		switch(oper) {
-		case MUL:
-			isUsed[idx][0] = true;
-			value = num1 * num2;
-			break;
-			
-		case DIV:
-			isUsed[idx][1] = true;
-			value = num1 / num2;
-			break;
-			
-		case ADD:
-			isUsed[idx][2] = true;
-			value = num1 + num2;
-			break;
-			
-		case SUB:
-			isUsed[idx][3] = true;
-			value = num1 - num2;
-			break;
-		}
-		
-		return value;
-	}
-
-	private static int nextCalculate(int a, int b, int idx) {
-		int val = 0;
-		
-		for(int i = 0; i < isUsed[idx].length; i++) {
-			if(isUsed[idx][i]) {
-				val = getValue(a, b, i);
-			}
-		}
-		
-		return val;
-	}
-	
-	private static int getValue(int a, int b, int op) {
-		if(op == 0) return a * b;
-		else if(op == 1) return a / b;
-		else if(op == 2) return a + b;
-		else return a - b;
+	private static int calculator(int num1, char oper, int num2) {
+		if(oper == MUL) return num1 * num2;
+		else if(oper == DIV) return num1 / num2;
+		else if(oper == ADD) return num1 + num2;
+		else return num1 - num2;
 	}
 }
