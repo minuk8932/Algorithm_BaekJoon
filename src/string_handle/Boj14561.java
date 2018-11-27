c2ffeb3195be40b1d800a5717cf58626ecd18b0a
minuk8932 <minuk8932@naver.com>
2018-11-27 15:43:29 +0900

    refactory String Handle(Palindrome) 181127

--- src/string_handle/Boj14561.java
+++ src/string_handle/Boj14561.java
@@ -1,7 +1,6 @@
 package string_handle;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -32,26 +31,23 @@ public class Boj14561 {
 			LinkedList<Character> word  = nBasedA(A, n);
 			sb.append(isPalindrome(word) ? 1 : 0).append(NEW_LINE);
 		}
-		
+
 		System.out.println(sb);		// 결과 출력
 	}
 	
 	private static LinkedList<Character> nBasedA(long num, int base) {
 		LinkedList<Character> str = new LinkedList<>();
-		ArrayDeque<Character> stack = new ArrayDeque();
 		
 		while(num > 0) {
 			char w = (char)((num % base) + C_TO_I);
 			
-			if(w > NUMBER_LIMIT) {										// w가 9보다 크다면 이후로 A ~ F(16진수까지)로 바꾸어줌
+			if(w > NUMBER_LIMIT) {										// w가 9보다 큰 경우 A ~ F(16진수까지)로 바꾸어줌
 				w = (char) ((w - NUMBER_LIMIT) + ALPHABET_A - 1);
 			}
 			
-			stack.push(w);
+            str.addFirst(w);
 			num /= base;
 		}
-		
-		while(!stack.isEmpty()) str.add(stack.pop());
 		
 		return str;
 	}
