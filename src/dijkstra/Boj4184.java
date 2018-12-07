008539af5700335e8fab131539f6404e43ebbbf3
minuk8932 <minuk8932@naver.com>
2018-12-07 14:08:33 +0900

    by rejudging.. Pause

--- src/dijkstra/Boj4184.java
+++ src/dijkstra/Boj4184.java
@@ -1,9 +1,7 @@
 package dijkstra;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 /**
  * 
@@ -20,52 +18,35 @@ public class Boj4184 {
 	private static final int INF = 1_000_001;
 
 	private static final char[] BEARING = {'0', '1', '2', '3', '4', '5', '6', '7'};
-	private static final String NEW_LINE = "\n";
-
-	private static int[][] cost = null;
 
 	public static void main(String[] args) throws Exception {
-		// 버퍼를 통한 값 입력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
 
 		char[][] map = new char[r + 1][c + 1];
 		for (int i = 1; i < r + 1; i++) {
-			String line = br.readLine();
-
+			String line = sc.next();
+			
 			for (int j = 0; j < c; j++) {
 				map[i][j + 1] = line.charAt(j);
 			}
 		}
 
-		int n = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
 
 		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
+            int startRow = sc.nextInt();
+            int startCol = sc.nextInt();
+            int endRow = sc.nextInt();
+            int endCol = sc.nextInt();
 
-			int startRow = Integer.parseInt(st.nextToken());			// 각 케이스 별 시작과 끝 정점을 저장
-			int startCol = Integer.parseInt(st.nextToken());
-			int endRow = Integer.parseInt(st.nextToken());
-			int endCol = Integer.parseInt(st.nextToken());
-
-			costInit(r, c);												// 각 케이스 별 비용 배열 초기화
-			dijkstra(r, c, startRow, startCol, endRow, endCol, map);	// 다익스트라 알고리즘 실행
-
-			sb.append(cost[endRow][endCol]).append(NEW_LINE);			// 다익스트라 알고리즘을 통해 나온 최소 비용을 버퍼에 저장
+			System.out.println(dijkstra(r, c, startRow, startCol, endRow, endCol, map));
 		}
-
-		System.out.println(sb.toString());				// 결과 값 한번에 출력
+        
+        sc.close();
 	}
 	
-	/**
-	 * 정점 이너 클래스
-	 * @author minchoba
-	 *
-	 */
 	private static class Point implements Comparable<Point> {
 		int row;
 		int col;
@@ -83,11 +64,8 @@ public class Boj4184 {
 		}
 	}
 	
-	/**
-	 *	다익스트라 알고리즘 메소드
-	 * 
-	 */
-	private static void dijkstra(int n, int m, int sRow, int sCol, int eRow, int eCol, char[][] map) {
+	private static int dijkstra(int n, int m, int sRow, int sCol, int eRow, int eCol, char[][] map) {
+        int[][] cost = costInit(n, m);
 		cost[sRow][sCol] = 0;
 
 		PriorityQueue<Point> pq = new PriorityQueue<>();
@@ -95,35 +73,33 @@ public class Boj4184 {
 
 		while (!pq.isEmpty()) {
 			Point current = pq.poll();
-			
-			for(int stare = 0; stare < BEARING.length; stare++) {
-				int nextRow = current.row + DIRECTIONS[stare][ROW];
-				int nextCol = current.col + DIRECTIONS[stare][COL];
+
+			for (int i = 0; i < 8; i++) {
+				int nextRow = current.row + DIRECTIONS[i][ROW];
+				int nextCol = current.col + DIRECTIONS[i][COL];
 				int nextCost = 0;
 
-				if (nextRow > 0 && nextRow < n + 1 && nextCol > 0 && nextCol < m + 1) {					
-					// 물의 흐름과 배가 가려는 방향이 같다면 0, 아니면 1을 다음 비용으로 저장
-					nextCost = (map[current.row][current.col] == BEARING[stare]) ? 0 : 1;
+				if (nextRow > 0 && nextRow < n + 1 && nextCol > 0 && nextCol < m + 1) {
+					nextCost = (map[current.row][current.col] == BEARING[i]) ? 0 : 1;
 
-					if (cost[nextRow][nextCol] > cost[current.row][current.col] + nextCost) {	// 다음 비용의 값이 현재까지 비용보다 큰 경우
-						cost[nextRow][nextCol] = cost[current.row][current.col] + nextCost;		// 값 갱신
+					if (cost[nextRow][nextCol] > cost[current.row][current.col] + nextCost) {	
+						cost[nextRow][nextCol] = cost[current.row][current.col] + nextCost;
 						
-						pq.offer(new Point(nextRow, nextCol, cost[nextRow][nextCol]));			// 다음 정점과 비용을 우선순위 큐에 담고 반복
+						pq.offer(new Point(nextRow, nextCol, cost[nextRow][nextCol]));
 					}
 				}
 			}
 		}
+        return cost[eRow][eCol];
 	}
+    
+	private static int[][] costInit(int r, int c) {
+		int[][] arr = new int[r + 1][c + 1];
 
-	/**
-	 * 비용 배열 초기화 메소드
-	 * 
-	 */
-	private static void costInit(int r, int c) {
-		cost = new int[r + 1][c + 1];
-
-		for (int i = 0; i < r + 1; i++) {	// 배열을 최댓값으로 초기화
-			Arrays.fill(cost[i], INF);
+		for (int i = 0; i < r + 1; i++) {
+			Arrays.fill(arr[i], INF);
 		}
+        
+        return arr;
 	}
 }
