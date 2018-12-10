9aaf38abcc11e245c85a09d45fffe0f157bb0849
minuk8932 <minuk8932@naver.com>
2018-12-10 12:17:17 +0900

    add Dijkstra(Ocean Currents) 181210

--- src/dijkstra/Boj4184.java
+++ src/dijkstra/Boj4184.java
@@ -1,14 +1,20 @@
 package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * 
- * @author minchoba
- * 백준 4184번: Ocean Current
- * 
- * @see https://www.acmicpc.net/problem/4184/
+ * 	@author minchoba
+ *	백준 4184번: Ocean Currents
+ *
+ *	@see https://www.acmicpc.net/problem/4184/
  *
  */
 public class Boj4184 {
@@ -19,32 +25,42 @@ public class Boj4184 {
 
 	private static final char[] BEARING = {'0', '1', '2', '3', '4', '5', '6', '7'};
 
+	private static final String NEW_LINE = "\n";
+
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();
-        int c = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int r = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 
 		char[][] map = new char[r + 1][c + 1];
 		for (int i = 1; i < r + 1; i++) {
-			String line = sc.next();
-			
+			String line = br.readLine();
+
 			for (int j = 0; j < c; j++) {
 				map[i][j + 1] = line.charAt(j);
 			}
 		}
 
-		int n = sc.nextInt();
+		int n = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < n; i++) {
-            int startRow = sc.nextInt();
-            int startCol = sc.nextInt();
-            int endRow = sc.nextInt();
-            int endCol = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
 
-			System.out.println(dijkstra(r, c, startRow, startCol, endRow, endCol, map));
+			int startRow = Integer.parseInt(st.nextToken());
+			int startCol = Integer.parseInt(st.nextToken());
+			int endRow = Integer.parseInt(st.nextToken());
+			int endCol = Integer.parseInt(st.nextToken());
+
+			int res = dijkstra(r, c, startRow, startCol, endRow, endCol, map);
+
+            bw.write(res + "\n");
 		}
-        
-        sc.close();
+		
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 	
 	private static class Point implements Comparable<Point> {
@@ -65,8 +81,8 @@ public class Boj4184 {
 	}
 	
 	private static int dijkstra(int n, int m, int sRow, int sCol, int eRow, int eCol, char[][] map) {
-        int[][] cost = costInit(n, m);
-		cost[sRow][sCol] = 0;
+		int[][] cost = costInit(n, m);
+        cost[sRow][sCol] = 0;
 
 		PriorityQueue<Point> pq = new PriorityQueue<>();
 		pq.offer(new Point(sRow, sCol, cost[sRow][sCol]));
@@ -80,7 +96,7 @@ public class Boj4184 {
 				int nextCost = 0;
 
 				if (nextRow > 0 && nextRow < n + 1 && nextCol > 0 && nextCol < m + 1) {
-					nextCost = (map[current.row][current.col] == BEARING[i]) ? 0 : 1;
+					nextCost = (map[current.row][current.col] == BEARING[i]) ? 0 : 1;		// 흐름과 물의 방향이 같으면 0 다르면 1
 
 					if (cost[nextRow][nextCol] > cost[current.row][current.col] + nextCost) {	
 						cost[nextRow][nextCol] = cost[current.row][current.col] + nextCost;
@@ -90,6 +106,7 @@ public class Boj4184 {
 				}
 			}
 		}
+        
         return cost[eRow][eCol];
 	}
     
