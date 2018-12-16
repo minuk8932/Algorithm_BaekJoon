06875ad7a2f51728bb89cf6eb95a62aa19fe6268
minuk8932 <minuk8932@naver.com>
2018-12-16 12:31:56 +0900

    mod String Handle(BigNumber A + B make methods) 181216

--- src/string_handle/Boj10757.java
+++ src/string_handle/Boj10757.java
@@ -14,61 +14,62 @@ import java.util.StringTokenizer;
  */
 public class Boj10757 {
 	public static void main(String[] args) throws Exception{
-		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		String n1 = st.nextToken();
 		String n2 = st.nextToken();
 		
-		int leng1 = n1.length();
-		int leng2 = n2.length();
+		calculator(n1, n2);
+	}
+	
+	private static void calculator(String A, String B) {
+		int leng1 = A.length();
+		int leng2 = B.length();
 		int leng = 0;
 		
 		char[] num1 = null;
 		char[] num2 = null;
 		
 		if(leng1 > leng2) {				// 길이가 짧은 것이 있다면 앞에 짧은 만큼 0을 붙여줌
-			num1 = n1.toCharArray();
-			num2 = new char[leng1];
-			
+			num1 = A.toCharArray();
 			leng = leng1;
 			
-			int idx = 0;
-			
-			for(int i = 0; i < leng1; i++) {
-				if(i < leng1 - leng2) {
-					num2[i] = '0';
-				}
-				else {
-					num2[i] = n2.charAt(idx++);
-				}
-			}
+			num2 = stringToCharArr(B, leng2, leng1);
 		}
 		else {
-			num2 = n2.toCharArray();
-			num1 = new char[leng2];
-			
+			num2 = B.toCharArray();
 			leng = leng2;
 			
-			int idx = 0;
-			
-			for(int i = 0; i < leng2; i++) {
-				if(i < leng2 - leng1) {
-					num1[i] = '0';
-				}
-				else {
-					num1[i] = n1.charAt(idx++);
-				}
+			num1 = stringToCharArr(A, leng1, leng2);
+		}
+		
+		System.out.println(makeBigInteger(leng, num1, num2));
+	}
+	
+	private static char[] stringToCharArr(String str, int leng1, int leng2) {
+		char[] arr = new char[leng2];
+		
+		int idx = 0;
+		for(int i = 0; i < leng2; i++) {
+			if(i < leng2 - leng1) {
+				arr[i] = '0';
+			}
+			else {
+				arr[i] = str.charAt(idx++);
 			}
 		}
 		
+		return arr;
+	}
+	
+	private static StringBuilder makeBigInteger(int length, char[] arr1, char[] arr2) {
 		Stack<Integer> res = new Stack<>();
 		int carry = 0;
 		
-		for(int i = leng - 1; i >= 0; i--) {
-			int tmp = (num1[i] - '0') + (num2[i] - '0') + carry;
+		for(int i = length - 1; i >= 0; i--) {
+			int tmp = (arr1[i] - '0') + (arr2[i] - '0') + carry;
 			
-			if(tmp > 9) {		// 더한 수를 스택에 넣어주면서, 9보다 커지는 경우엔 carry를 1로 바꾸어 계산
+			if(tmp > 9) {
 				tmp -= 10;
 				res.push(tmp);
 				
@@ -80,11 +81,11 @@ public class Boj10757 {
 			}
 		}
 		
-		if(carry == 1) res.push(1);				// 마지막에 carry가 존재 할 경우 스택에 1 push
+		if(carry == 1) res.push(1);				// 마지막에 carry가 존재 할 경우
 		
 		StringBuilder sb = new StringBuilder();
-		while(!res.isEmpty()) sb.append(res.pop());		// 숫자를 역순으로 뽑으며 버퍼에 담아줌
+		while(!res.isEmpty()) sb.append(res.pop());
 		
-		System.out.println(sb.toString());		// 결과 값 한번에 출력
+		return sb;
 	}
 }
