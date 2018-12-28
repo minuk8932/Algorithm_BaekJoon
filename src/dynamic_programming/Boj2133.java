6fb338d5d79d0365df9682b4fc67ef470154b6bd
minuk8932 <minuk8932@naver.com>
2018-12-28 15:23:26 +0900

    add DP(Tile Addition) 181228

--- src/dynamic_programming/Boj2133.java
+++ src/dynamic_programming/Boj2133.java
@@ -1,3 +1,4 @@
+package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -23,28 +24,27 @@ public class Boj2133 {
 	 *  
 	 */
 	public static void main(String[] args) throws Exception{
-		//버퍼를 이용한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		br.close();
 		
-		if(N % 2 == 1){					// 만약 홀수가 들어올 경우 : 존재 x ... i)
+		if(N % 2 == 1){
 			System.out.println(0);
 			return;
 		}
-		else{									// 짝수의 경우
+		else{
 			int[] dp = new int[MAX + 1];
-			dp[0] = 1;		// 곱셈에 필요한 값 : 1로 설정
+			dp[0] = 1;
 			dp[2] = 3;
 			
 			for (int i = 4; i < N + 1; i += 2){
-		        dp[i] = dp[i - 2] * 3;		// 규칙적인 타일이 나오는 경우 
+		        dp[i] = dp[i - 2] * 3;			// 규칙적인 타일
 		        
 		        for (int j = 4; j < i + 1; j += 2){
-		            dp[i] += dp[i - j] * 2;	// 불규칙적인 타일의 경우 ... ii)
+		            dp[i] += dp[i - j] * 2;		// 불규칙적인 타일 ... ii)
 		        }
 		    }
-			System.out.println(dp[N]);	// 결과값 출력
+			System.out.println(dp[N]);	// 결과 출력
 		}
 	}
 }
