0cb802fba431ec92f5f42eba41166dd7556f92d7
minuk8932 <minuk8932@naver.com>
2019-02-13 15:22:00 +0900

    refactoring BFS(Tomato) 190213

--- src/breadth_first_search/Boj7576.java
+++ src/breadth_first_search/Boj7576.java
@@ -16,16 +16,23 @@ import java.util.StringTokenizer;
  */
 public class Boj7576 {
 	public static final String SPACE = " ";
-	private static final int RIPED = 1;
-	private static final int RARED = 0;
-	private static final int NULL = -1;
+	private static final int[] STATE = {-1, 0, 1};
 	
 	private static final int[][] DIRECTIONS = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 	private static final int ROW = 0;
 	private static final int COL = 1;
+	
+	private static class Point {
+		int row;
+		int col;
+
+		public Point(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
-		//	버퍼를 통한 값 입력 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
 
@@ -41,71 +48,51 @@ public class Boj7576 {
 		for(int row = 0; row < N; row++){
 			st = new StringTokenizer(br.readLine(), SPACE);
 			
-			for(int col = 0; col < M; col++){						// 익은것, 안익은것, 비어있는것을 차례로 담아줌
-				switch(Integer.parseInt(st.nextToken())){
-				case RIPED :
-					map[row][col] = RIPED;
+			for(int col = 0; col < M; col++){
+				int tomato = Integer.parseInt(st.nextToken());
+				
+				if(tomato == STATE[0]) {
+					map[row][col] = STATE[0];
+				}
+				else if(tomato == STATE[1]) {
+					raredCnt++;
+				}
+				else {
+					map[row][col] = STATE[2];
 					queue.offer(new Point(row, col));
 					isVisited[row][col] = 1;
-					break;
-					
-				case RARED :
-					map[row][col] = RARED;
-					raredCnt++;					
-					break;
-					
-				case NULL :
-					map[row][col] = NULL;
-					break;
 				}
 			}
 		}
 		
 		br.close();
-		
+		System.out.println(bfs(N, M, queue, map, isVisited, raredCnt));
+	}
+	
+	private static int bfs(int n, int m, Queue<Point> q, int[][] map, int[][] visit, int count) {
 		int maxDays = 1;
 		
-		while(!queue.isEmpty()){							// BFS 알고리즘 실행
-			Point current = queue.poll();
+		while(!q.isEmpty()){							// 토마토 위치별로 BFS 알고리즘 실행
+			Point current = q.poll();
 			
 			for(final int[] DIRECTION : DIRECTIONS){
 				int nextRow = current.row + DIRECTION[ROW];
 				int nextCol = current.col + DIRECTION[COL];
 				
-				if(nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < M){
-					if(map[nextRow][nextCol] == RARED && isVisited[nextRow][nextCol] == 0){
+				if(nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < m){
+					if(map[nextRow][nextCol] == STATE[1] && visit[nextRow][nextCol] == 0){
 						
-						queue.offer(new Point(nextRow, nextCol));
-						map[nextRow][nextCol] = RIPED;														// 익은것이다 표시
-						isVisited[nextRow][nextCol] = isVisited[current.row][current.col] + 1; // 단계별 계산
-						maxDays = Math.max(maxDays, isVisited[nextRow][nextCol]);			// 최대 일수 계산
-						raredCnt--;
+						q.offer(new Point(nextRow, nextCol));
+						map[nextRow][nextCol] = STATE[2];														// 익은것
+						visit[nextRow][nextCol] = visit[current.row][current.col] + 1; 
+						maxDays = Math.max(maxDays, visit[nextRow][nextCol]);			// 최대 일수
+						count--;
 					}
 				}
 			}
 		}
-		if(raredCnt == 0){								// 다 익었으면 걸린 익는데 날짜를 출력 
-			System.out.println(maxDays - 1);
-		}
 		
-		else{													// 안익은게 있다면 -1 출력
-			System.out.println(NULL);
-		}
-		
-	}
-
-	/**
-	 * 
-	 * 	@author minchoba
-	 *	정점 이너 클래스
-	 */
-	private static class Point {
-		int row;
-		int col;
-
-		public Point(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
+		if(count == 0)	return maxDays -1;	// 다 익었으면 걸린 익는데 날짜를 출력 
+		else return STATE[0];				// 안익은게 있다면 -1 출력
 	}
 }
