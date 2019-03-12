d0fd5465a6b04aa6322c3ca1b38f67f9bb0f236f
minuk8932 <minuk8932@naver.com>
2019-03-12 14:16:24 +0900

    add Hasing(SHA-384) 190312

--- src/hashing/Boj10931.java
+++ src/hashing/Boj10931.java
@@ -1,8 +1,17 @@
+package hashing;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * 
+ * 	@author minchoba
+ *	백준 10931번: SHA-384
+ *
+ *	@see https://www.acmicpc.net/problem/10931/
+ *
+ */
 public class Boj10931 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,17 +22,17 @@ public class Boj10931 {
 		StringBuilder sb = new StringBuilder();
 		byte[] crypto = sha384(str);
 		
-		for(byte b: crypto) {
+		for(byte b: crypto) {						// 각 바이트를 Hexa로 바꿔줌 (byteToHex)
 			sb.append(String.format("%02x", b));
 		}
 		
-		return sb.toString();
+		return sb.toString();			// 생성된 암호화 문자열 반환
 	}
 	
 	private static byte[] sha384(String msg) throws NoSuchAlgorithmException{
-		MessageDigest md = MessageDigest.getInstance("SHA-384");
+		MessageDigest md = MessageDigest.getInstance("SHA-384");				// 문자열을 Security Hashing Algorithm 384로 바이트 배열화
 		md.update(msg.getBytes());
 		
-		return md.digest();
+		return md.digest();			// 바이트 배열 반환
 	}
 }
