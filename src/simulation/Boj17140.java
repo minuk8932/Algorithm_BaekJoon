5a0a8450f333afc4290d912255e3b8c667515cd1
minuk8932 <minuk8932@naver.com>
2019-04-15 17:18:33 +0900

    refactor Simulation(Matrix & Query) 190415

--- src/simulation/Boj17140.java
+++ src/simulation/Boj17140.java
@@ -13,6 +13,8 @@ import java.util.StringTokenizer;
  *
  */
 public class Boj17140 {
+	private static int[][] matrix;
+	
 	private static class Pair implements Comparable<Pair>{
 		int value;
 		int cost;
@@ -45,7 +47,7 @@ public class Boj17140 {
 		int c = Integer.parseInt(st.nextToken()) - 1;
 		int k = Integer.parseInt(st.nextToken());
 		
-		int[][] matrix = new int[100][100];
+		matrix = new int[100][100];
 		for(int i = 0; i < 3; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j = 0; j < 3; j++) {
@@ -53,80 +55,22 @@ public class Boj17140 {
 			}
 		}
 		
-		System.out.println(makeArray(r, c, k, matrix));
+		System.out.println(makeArray(r, c, k));
 	}
 	
-	private static int makeArray(int R, int C, int K, int[][] arr) {
-		int result = 0;
-		PriorityQueue<Pair> pq;
-		
+	private static int makeArray(int R, int C, int K) {		
 		int rSize = 3, cSize = 3;
+		int result = 0;
 		
-		while(arr[R][C] != K) {			
+		while(matrix[R][C] != K) {			
 			int max = 0;
 			
 			if(rSize >= cSize) {				
-				for(int i = 0; i < rSize; i++) {
-					pq = new PriorityQueue<>();
-					
-					int[] count = new int[101];
-					for(int j = 0; j < cSize; j++) {
-						count[arr[i][j]]++;			// 등장하는 숫자의 갯수
-					}
-					
-					int resize = 0;
-					for(int x = 1; x < 101; x++) {
-						if(count[x] == 0) continue;
-						pq.offer(new Pair(x, count[x]));
-						resize++;
-					}
-					
-					resize *= 2;							// 갱신 사이즈
-					max = Math.max(resize, max);			// 최대 사이즈 -> 추후 부분 연산 후 col 크기가 됨
-					
-					for(int j = 0; j < resize; j += 2) {	// 배열 재구성
-						Pair current = pq.poll();
-						arr[i][j] = current.value;
-						arr[i][j + 1] = current.cost;
-					}
-					
-					for(int j = resize; j < 100; j++) {		// 범위 밖 잔여 숫자 초기화
-						arr[i][j] = 0;
-					}
-				}
-				
-				cSize = max;								// col 사이즈 갱신
+				max = Math.max(remakeMatrix(rSize, cSize, true), max);
+				cSize = max;
 			}
 			else {
-				for(int i = 0; i < cSize; i++) {			// row 연산과 같음
-					pq = new PriorityQueue<>();
-					
-					int[] count = new int[101];
-					for(int j = 0; j < rSize; j++) {
-						count[arr[j][i]]++;
-					}
-					
-					int resize = 0;
-					for(int x = 1; x < 101; x++) {
-						if(count[x] == 0) continue;
-						pq.offer(new Pair(x, count[x]));
-						resize++;
-					}
-					
-					resize *= 2;
-					max = Math.max(resize, max);
-					
-					for(int j = 0; j < resize; j += 2) {
-						Pair current = pq.poll();
-						arr[j][i] = current.value;
-						arr[j + 1][i] = current.cost;
-					}
-					
-					for(int j = resize; j < 100; j++) {
-						arr[j][i] = 0;
-					}
-				}
-				
+				max = Math.max(remakeMatrix(cSize, rSize, false), max);
 				rSize = max;
 			}
 			
@@ -134,7 +78,49 @@ public class Boj17140 {
 			if(result > 100) return -1;			// 100번 연산 후 값이 안나온 경우
 		}
 		
-		
 		return result;
+	}
+	
+	private static int remakeMatrix(int r, int c, boolean flag) {		
+		PriorityQueue<Pair> pq;
+		int size = 0;
+		
+		for(int i = 0; i < r; i++) {
+			pq = new PriorityQueue<>();
+			
+			int[] count = new int[101];
+			for(int j = 0; j < c; j++) {
+				count[flag ? matrix[i][j]: matrix[j][i]]++;			// 등장하는 숫자의 갯수
+			}
+			
+			int resize = 0;
+			for(int x = 1; x < 101; x++) {
+				if(count[x] == 0) continue;
+				pq.offer(new Pair(x, count[x]));
+				resize += 2;
+			}
+			
+			size = Math.max(resize, size);			// 최대 사이즈 -> 추후 부분 연산 후 r 크기가 됨
+			
+			for(int j = 0; j < resize; j += 2) {	// 배열 재구성
+				Pair current = pq.poll();
+				
+				if(flag) {
+					matrix[i][j] = current.value;
+					matrix[i][j + 1] = current.cost;
+				}
+				else {
+					matrix[j][i] = current.value;
+					matrix[j + 1][i] = current.cost;
+				}
+			}
+			
+			for(int j = resize; j < 100; j++) {		// 범위 밖 잔여 숫자 초기화
+				if(flag) matrix[i][j] = 0;
+				else matrix[j][i] = 0;
+			}
+		}
+		
+		return size;
 	}
 }
