efab5359f5bcadf59f30de5079254c8c55b17d37
minuk8932 <minuk8932@naver.com>
2019-05-05 14:01:34 +0900

    add StringHandle(Decoder) 190505

--- src/string_handle/Boj17176.java
+++ src/string_handle/Boj17176.java
@@ -1,8 +1,9 @@
+package string_handle;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BojContestD {
+public class Boj17176 {
 	private static final char SPACE = ' ';
 	
 	public static void main(String[] args) throws Exception{
