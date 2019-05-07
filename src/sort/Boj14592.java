d6ee634884a27e70e2e68d29dda2c98b89873e50
minuk8932 <minuk8932@naver.com>
2019-05-07 19:47:53 +0900

    add Sort(2017 APC small) 190507

---
+++ src/sort/Boj14592.java
@@ -0,0 +1,65 @@
+package sort;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 	@author minchoba
+ *	백준 14592번: 2017 아주대학교 프로그래밍 경시대회 (Small)
+ *
+ *	@see https://www.acmicpc.net/problem/14592/
+ *
+ */
+public class Boj14592 {
+	private static class Participant implements Comparable<Participant>{
+		int idx;
+		int score;
+		int report;
+		int time;
+		
+		public Participant(int idx, int score, int report, int time) {
+			this.idx = idx;
+			this.score = score;
+			this.report = report;
+			this.time = time;
+		}
+
+		@Override
+		public int compareTo(Participant p) {
+			if(this.score > p.score) {
+				return -1;
+			}
+			else if(this.score < p.score) {
+				return 1;
+			}
+			else {
+				if(this.report < p.report) {
+					return -1;
+				}
+				else if(this.report > p.report) {
+					return 1;
+				}
+				else {
+					if(this.time < p.time) return -1;
+					else if(this.time > p.time) return 1;
+					else return 0;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Participant> pq = new PriorityQueue<>();
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			pq.offer(new Participant(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+		
+		System.out.println(pq.poll().idx + 1);		// 1등
+	}
+}
