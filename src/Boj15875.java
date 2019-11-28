62c263e8f726ea8ee0f2f67543a237230ecc5ffb
mibnuk8932 <minuk8932@naver.com>
2019-11-28 16:40:49 +0900

    add ConvexHull(Poison Barrier) 191128

--- src/Boj15875.java
+++ src/Boj15875.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -9,13 +12,29 @@ public class Boj15875 {
 	private static final int ROW = 0;
 	private static final int COL = 1;
 	
-	private static class Point{
+	private static int[] parent;
+	private static ArrayList<Point> start = new ArrayList<>();
+	private static ArrayList<Point> mins = new ArrayList<>();
+	
+	private static class Point implements Comparable<Point>{
 		int row;
 		int col;
+		int val;
 		
 		public Point(int row, int col) {
 			this.row = row;
 			this.col = col;
+		}
+		
+		public Point(int row, int col, int val) {
+			this.row = row;
+			this.col = col;
+			this.val = val;
+		}
+
+		@Override
+		public int compareTo(Point p) {
+			return this.val < p.val ? -1: 1;
 		}
 	}
 	
@@ -25,17 +44,92 @@ public class Boj15875 {
 		int W = in.readInt();
 		
 		int[][] map = new int[H][W];
+		parent = new int[H * W];
+		Arrays.fill(parent, -1);
 		
-		int max = 0;
+		int min = Integer.MAX_VALUE;
 		
 		for(int i = 0; i < H; i++) {
 			for(int j = 0; j < W; j++) {
 				map[i][j] = in.readInt();
-				if(map[i][j] > max) max = map[i][j];
+				start.add(new Point(i, j, map[i][j]));
+				
+				if(min > map[i][j]) min = map[i][j];
 			}
 		}
 		
-		System.out.println();
+		Collections.sort(start);
+		System.out.println(bfs(H, W, map));
+	}
+	
+	private static int bfs(int h, int w, int[][] arr) {
+		boolean[][] visit = new boolean[h][w];
+		Queue<Point> q = new LinkedList<>();
+		
+		int max = 0;
+		
+		for(Point s: start) {
+			if(visit[s.row][s.col]) continue;
+			visit[s.row][s.col] = true;
+			
+			q.offer(new Point(s.row, s.col));
+			int par = s.row * w + s.col;
+			
+			while(!q.isEmpty()) {
+				Point current = q.poll();
+				
+				for(final int[] DIRECTION: DIRECTIONS) {
+					int nextRow = current.row + DIRECTION[ROW];
+					int nextCol = current.col + DIRECTION[COL];
+					
+					if(nextRow < 0 || nextRow >= h || nextCol < 0 || nextCol >= w) {
+						parent[current.row * w + current.col] = -1;
+						continue;
+					}
+					
+					if(arr[nextRow][nextCol] > s.val) continue;
+					
+					if(visit[nextRow][nextCol]) continue;
+					visit[nextRow][nextCol] = true;
+					
+					merged(par, nextRow * w + nextCol);
+					max = Math.max(max, -parent[find(par)]);
+					
+					for(int i = 0; i < parent.length; i++) {
+						if(i % w == 0) System.out.println();
+						System.out.print(parent[i] + " ");
+					}
+					System.out.println();
+					
+					q.offer(new Point(nextRow, nextCol));
+				}
+			}
+		}
+		
+		return max;
+	}
+	
+	private static int find(int x) {
+		if(parent[x] < 0) return x;
+		else return parent[x] = find(parent[x]);
+	}
+	
+	private static boolean merged(int x, int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x == y) return true;
+		
+		if(parent[x] < parent[y]) {
+			parent[x] += parent[y];
+			parent[y] = x;
+		}
+		else {
+			parent[y] += parent[x];
+			parent[x] = y;
+		}
+		
+		return false;
 	}
 	
 	private static class InputReader {
---
+++ src/convex_hull/Boj7420.java
@@ -0,0 +1,137 @@
+package convex_hull;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/**
+ * 
+ * 	@author exponential-e
+ *	백준 7420번: 맹독 방벽
+ *
+ *	@see https://www.acmicpc.net/problem/7420/
+ *
+ */
+public class Boj7420 {
+	private static ArrayDeque<Point> stack = new ArrayDeque<>();
+	private static final int INF = 1_000_000_000;
+	private static final double PI = Math.PI;
+	
+	private static Point min = new Point(INF, INF);
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		Point[] wall = new Point[N];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			if (y < min.y) {					// get target
+				min = new Point(x, y);
+			}
+
+			else if (y == min.y) {
+				if (x < min.x) {
+					min = new Point(x, y);
+				}
+			}
+			
+			wall[i] = new Point(x, y);
+		}
+		
+		Arrays.sort(wall, Point.comparator);
+		convexHull(N, wall);
+		
+		System.out.println(getDistance(L));
+	}
+	
+	private static class Point {
+		long x;
+		long y;
+
+		public Point(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		private static Comparator<Point> comparator = new Comparator<Point>() {
+			@Override
+			public int compare(Point p1, Point p2) {
+				long c = CCW(min, p1, p2);
+	
+				if (c > 0) {
+					return -1;
+				}
+				
+				else if (c < 0) {
+					return 1;
+				}
+				
+				else if (c == 0) {
+					long a = distancePow(min, p1);
+					long b = distancePow(min, p2);
+					
+					if (a < b) return -1;
+					else if (a > b) return 1;
+					else if (a == b) return 0;
+				}
+				
+				return 0;
+			}
+		};
+	}
+	
+	private static long CCW(Point v1, Point v2, Point v3) {
+		return (v1.x * v2.y + v2.x * v3.y + v3.x * v1.y) - (v3.x * v2.y + v2.x * v1.y + v1.x * v3.y);
+	}
+	
+	private static long distancePow(Point p1, Point p2) {
+		return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+	}
+	
+	private static void convexHull(int N, Point[] p) {
+		stack.push(new Point(p[0].x, p[0].y));
+		stack.push(new Point(p[1].x, p[1].y));
+
+		for (int idx = 2; idx < N; idx++) {
+			Point next = new Point(p[idx].x, p[idx].y);
+
+			while (stack.size() >= 2) {
+				Point second = stack.pop();
+				Point first = stack.peek();
+
+				if (CCW(first, second, next) > 0) {
+					stack.push(second);
+					break;
+				}
+			}
+
+			stack.push(next);
+		}
+	}
+	
+	private static long getDistance(int limit) {
+		int size = stack.size();
+		double totalLen = (PI * size - (PI * (size - 2))) * limit;			// to find the circumference of a circle
+		
+		Point prev = stack.pop();
+		Point start = prev;
+		
+		while(!stack.isEmpty()) {
+			Point current = stack.pop();
+			totalLen += Math.sqrt(distancePow(prev, current));				// spare length
+			
+			prev = current;
+		}
+		
+		totalLen += Math.sqrt(distancePow(start, prev));
+		return Math.round(totalLen);
+	}
+}
