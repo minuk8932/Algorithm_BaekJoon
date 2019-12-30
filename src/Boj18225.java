af60726bdd312487a6d57d24f397273ecdc71189
mibnuk8932 <minuk8932@naver.com>
2019-12-30 15:44:35 +0900

    add BinarySearch(Standing long jump) 191230

---
+++ src/Boj18225.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj18225 {
+	
+	private static class Line{
+		int x;
+		int y;
+		int dx;
+		int dy;
+		
+		public Line(int x, int y, int dx, int dy) {
+			this.x = x;
+			this.y = y;
+			this.dx = dx;
+			this.dy = dy;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
--- src/binary_search/Boj6209.java
+++ src/binary_search/Boj6209.java
@@ -1,8 +1,17 @@
+package binary_search;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * 	@author exponential-e
+ *	백준 6209번: 제자리 멀리 뛰기
+ *
+ *	@see https://www.acmicpc.net/problem/6209/
+ *
+ */
 public class Boj6209 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,15 +32,27 @@ public class Boj6209 {
 	}
 	
 	private static int binarySearch(int n, int m, int[] arr) {
-		int end = arr[n - 1];
-		int start = 0;
+		int end = arr[n] + 1;
+		int start = 0, res = 0;
 		
 		while(start <= end) {
 			int mid = (start + end) / 2;
+			int count = 0, prev = 0;
 			
+			for(int i = 0; i < arr.length; i++) {
+				if(arr[i] - prev < mid) count++;			// check interval
+				else prev = arr[i];
+			}
 			
+			if(count > m) {
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+				res = mid;									// find min
+			}
 		}
 		
-		return 0;
+		return res;
 	}
 }
