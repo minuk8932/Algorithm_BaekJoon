68768b98ea4b8c25949c4105954f8f07852191b8
mibnuk8932 <minuk8932@naver.com>
2020-01-25 18:08:04 +0900

    add Dijkstra(Talent is Hard) 200125

---
+++ src/Boj16496.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj16496 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
--- src/dijkstra/Boj17270.java
+++ src/dijkstra/Boj17270.java
@@ -1,15 +1,22 @@
+package dijkstra;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * 	@author exponential-e
+ *	백준 17270번: 연예인은 힘들어
+ *
+ *	@see https://www.acmicpc.net/problem/17270/
+ *
+ */
 public class Boj17270 {
 	private static ArrayList<Node>[] path;
-	
-	private static boolean[] candidate;
-	private static int[] dist;
-	private static int INF = 10_000_001;
+	private static final int INF = 1_000_000_001;
 	
 	private static class Node implements Comparable<Node>{
 		int node;
@@ -33,11 +40,8 @@ public class Boj17270 {
 		int M = Integer.parseInt(st.nextToken());
 		
 		path = new ArrayList[V];
-		dist = new int[V];
-		candidate = new boolean[V];
 		for(int i = 0; i < V; i++) {
 			path[i] = new ArrayList<>();
-			dist[i] = INF;
 		}
 		
 		while(M-- > 0) {
@@ -54,30 +58,57 @@ public class Boj17270 {
 		int J = Integer.parseInt(st.nextToken()) - 1;
 		int S = Integer.parseInt(st.nextToken()) - 1;
 		
-		dijkstra(J, S);
-		bfs(J, S);
+		int[][] result = new int[2][V];
+		
+		result[0] = dijkstra(J, V);
+		result[1] = dijkstra(S, V);
+		
+		int cost = INF;
+		int spec = INF;
+		int index = -1;
+		
+		for (int e = 0; e < V; e++) {
+			if (e == J || e == S || result[0][e] == INF || result[1][e] == INF) continue;
+
+			int sum = result[0][e] + result[1][e];
+			cost = Math.min(cost, sum);									// find the shortest path
+		}
+		
+		for (int e = 0; e < V; e++) {
+			if (e == J || e == S || result[0][e] == INF || result[1][e] == INF) continue;
+			
+			int sum = result[0][e] + result[1][e];
+			if (cost != sum || result[0][e] > result[1][e]) continue;
+
+			if (spec >= result[0][e]) {									// find min j, min idx
+				spec = result[0][e];
+				index = e;
+			}
+		}
+
+		System.out.print(index == -1 ? -1: index + 1);
 	}
 	
-	private static void dijkstra(int start, int end) {
+	private static int[] dijkstra(int start, int size) {
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.offer(new Node(start, 0));
 		
-		dist[start] = 0;
+		int[] dist = new int[size];
+		Arrays.fill(dist, INF);
 		
 		while(!pq.isEmpty()) {
 			Node current = pq.poll();
+			if(current.cost > dist[current.node]) continue;
 			
-			for(Node next: path[current.node]) {
-				if(dist[next.node] > next.cost + dist[current.node]) {
-					dist[next.node] = next.cost + dist[current.node];
+			for(Node next: path[current.node]) {				
+				if(dist[next.node] > next.cost + current.cost) {
+					dist[next.node] = next.cost + current.cost;
 					
 					pq.offer(new Node(next.node, dist[next.node]));
 				}
 			}
 		}
-	}
-	
-	private static void bfs(int start, int end) {
 		
+		return dist;
 	}
 }
