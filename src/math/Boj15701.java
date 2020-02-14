de071d04da7674005cc3a094dea7a4b0b6ca23f9
mibnuk8932 <minuk8932@naver.com>
2020-02-14 17:56:19 +0900

    add Math(sequence pair) 200214

---
+++ src/math/Boj15701.java
@@ -0,0 +1,34 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 15701번: 순서쌍
+ *
+ * @see https://www.acmicpc.net/problem/15701/
+ *
+ */
+public class Boj15701 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(counting(N));
+    }
+
+    private static int counting(int n){
+        int loop = (int) (Math.sqrt(n));
+        int count = 2;
+
+        if(loop * loop == n) count++;
+
+        for(int i = 2; i < loop; i++){
+            if(n % i == 0) count += 2;
+        }
+
+        return count;
+    }
+}
