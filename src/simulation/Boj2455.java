116850cfc7b3aa1679befad2fcd314ea1652756a
mibnuk8932 <minuk8932@naver.com>
2020-03-06 20:04:34 +0900

    modify Simulation(Intelligent Train) 200306

--- src/simulation/Boj2455.java
+++ src/simulation/Boj2455.java
@@ -14,6 +14,7 @@ import java.util.StringTokenizer;
  */
 public class Boj2455 {
 	private static final int TRAINS = 4;
+	private static final int MAX = 10_000;
 	private static final String SPACE = " ";
 	
 	public static void main(String[] args) throws Exception{
@@ -22,17 +23,21 @@ public class Boj2455 {
 		int[] take = new int[TRAINS + 1];
 		int[] off = new int[TRAINS + 1];
 		int res = 0;
+		int p = 0;
 		
 		for(int i = 1; i < TRAINS + 1; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
 			off[i] = Integer.parseInt(st.nextToken());
 			take[i] = Integer.parseInt(st.nextToken());
-			
-			res = Math.max(res, res + take[i] - off[i]);
+
+			p -= off[i];
+			p += take[i];
+
+			if(p > MAX) p = MAX;
+			if(p > res) res = p;
 		}
-		br.close();
 		
-		System.out.println(res);							// 결과 값 출력
+		System.out.println(res);
 		
 	}
 }
