08200935877b01a982a9b843bf66b90c6050fb3c
mibnuk8932 <minuk8932@naver.com>
2020-03-19 14:29:02 +0900

    add BinarySearch(Binary search tree) 200319

--- src/binary_search/Boj1539.java
+++ src/binary_search/Boj1539.java
@@ -1,18 +1,28 @@
+package binary_search;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Boj1593 {
+/**
+ *
+ * @author exponential-e
+ * 백준 1539번: 이진 검색 트리
+ *
+ * @see https://www.acmicpc.net/problem/1539/
+ *
+ */
+public class Boj1539 {
     public static List<Integer> list = new ArrayList<>();
-    private static long[] h;
+    private static long[] height;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         long sum = 0;
 
-        h = new long[N];
+        height = new long[N];
         for(int i = 0; i < N; i++){
             int node = Integer.parseInt(br.readLine());
             sum += getHeight(node);
@@ -21,11 +31,11 @@ public class Boj1593 {
         System.out.println(sum);
     }
 
-    private static int binarySearch(int start, int end, int key){
+    private static int binarySearch(int start, int end, int target){
         while(start < end){
             int mid = (start + end) / 2;
 
-            if(list.get(mid) < key) start = mid + 1;
+            if(list.get(mid) < target) start = mid + 1;
             else end = mid;
         }
 
@@ -34,14 +44,16 @@ public class Boj1593 {
 
     private static long getHeight(int node){
         int size = list.size();
-        int target = binarySearch(0, size, node);
+        int index = binarySearch(0, size, node);          // find position
 
-        long left = target > 0 ? h[list.get(target - 1)] : 0;
-        long right = target < size ? h[list.get(target)] : 0;
+        long left = 0, right = 0;
 
-        h[node] = Math.max(left, right)+1;
-        list.add(target, node);
+        if(index > 0) left = height[list.get(index - 1)];
+        if(index < size) right = height[list.get(index)];
 
-        return h[node];
+        height[node] = Math.max(left, right) + 1;               // set height
+        list.add(index, node);                                  // add node
+
+        return height[node];
     }
 }
