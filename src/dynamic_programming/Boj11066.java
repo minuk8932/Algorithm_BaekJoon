14703360a08369d2a30635db591614d3a2b4429a
mibnuk8932 <minuk8932@naver.com>
2020-03-24 17:22:29 +0900

    add DP(Merging files) 200324

--- src/dynamic_programming/Boj11066.java
+++ src/dynamic_programming/Boj11066.java
@@ -1,7 +1,17 @@
+package dynamic_programming;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ *
+ * @author exponential-e
+ * 백준 11066번: 파일 합치기
+ *
+ * @see https://www.acmicpc.net/problem/11066/
+ *
+ */
 public class Boj11066 {
 	private static final String NEW_LINE = "\n";
 	
@@ -13,34 +23,33 @@ public class Boj11066 {
 		while(T-- > 0) {
 			int N = Integer.parseInt(br.readLine());
 			int[] files = new int[N + 1];
-			int[] sum = new int[N + 1];
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 1; i < N + 1; i++) {
+			for(int i = 1; i <= N; i++) {
 				files[i] = Integer.parseInt(st.nextToken());
-				sum[i] = sum[i - 1] + files[i];
+				files[i] += files[i - 1];							// prefix sum
 			}
 			
-			sb.append(get(N, files, sum)).append(NEW_LINE);
+			sb.append(get(N, files)).append(NEW_LINE);
 		}
 		
 		System.out.println(sb.toString());
 	}
 	
-	private static long get(int n, int[] arr, int[] sum) {
-		long[][] dp = new long[n + 1][n + 1];
-		
+	private static int get(int n, int[] f) {
+		int[][] dp = new int[n + 1][n + 1];
+
 		for(int i = 1; i < n; i++) {
-			for(int x = 1; x + i <= n; x++) {
+			for(int x = 1; x + i <= n; x++) {					// [~ x - 1][y ~]
 				int y = x + i;
-				
-				dp[x][y] = Long.MAX_VALUE;
+
+				dp[x][y] = Integer.MAX_VALUE;
 				for(int z = x; z < y; z++) {
-					dp[x][y] = Math.min(dp[x][y], dp[x][z] + dp[z + 1][y] + sum[y] - sum[x - 1]); 
+					dp[x][y] = Math.min(dp[x][y], dp[x][z] + dp[z + 1][y] + f[y] - f[x - 1]);		// make next size segment sum
 				}
 			}
 		}
-		
+
 		return dp[1][n];
 	}
 }
