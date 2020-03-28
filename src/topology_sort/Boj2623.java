318e4ab3d93c9c6f4bd6e0a8b94f1c66ad7eaf1b
mibnuk8932 <minuk8932@naver.com>
2020-03-28 17:03:01 +0900

    refactor TopologySort(Music program) 200328

--- src/topology_sort/Boj2623.java
+++ src/topology_sort/Boj2623.java
@@ -1,87 +1,106 @@
 package topology_sort;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
- * 
- * 	@author minchoba
- *	백준 2623번: 음악 프로그램
  *
- *	@see https://www.acmicpc.net/problem/2623/
+ * @author exponential-e
+ * 백준 2623번: 음악 프로그램
+ *
+ * @see https://www.acmicpc.net/problem/2623/
  *
  */
 public class Boj2623 {
-	private static ArrayDeque<Integer> stack = new ArrayDeque<>();
-	private static boolean[] isVisited;
-	private static boolean[] isFinished;
-	private static boolean isCycle;
-	
 	private static final String NEW_LINE = "\n";
-	
+
+	private static int[] indegree;
+	private static ArrayList<Integer>[] seq;
+
+	private static class Pair implements Comparable<Pair>{
+		int idx;
+		int cnt;
+
+		public Pair(int idx, int cnt){
+			this.idx = idx;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Pair p) {
+			if (this.cnt < p.cnt) return -1;
+			else if (this.cnt > p.cnt) return 1;
+			else  return 0;
+		}
+	}
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer>[] singer = new ArrayList[N + 1];
-		isFinished = new boolean[N + 1];
-		isVisited = new boolean[N + 1];
-		
-		for(int i = 1; i < N + 1; i++) {
-			singer[i] = new ArrayList<>();
+
+		seq = new ArrayList[N];
+		indegree = new int[N];
+
+		for(int i = 0; i < N; i++){
+			seq[i] = new ArrayList<>();
 		}
-		
-		while(M-- > 0) {
+
+		while (M-- > 0){
 			st = new StringTokenizer(br.readLine());
 			int count = Integer.parseInt(st.nextToken());
-			
-			int[] seq = new int[count];
+
+			int[] s = new int[count];
 			for(int i = 0; i < count; i++) {
-				seq[i] = Integer.parseInt(st.nextToken());
+				s[i] = Integer.parseInt(st.nextToken()) - 1;
 			}
-			
+
 			for(int i = 0; i < count - 1; i++) {
-				singer[seq[i]].add(seq[i + 1]);
+				seq[s[i]].add(s[i + 1]);
+				indegree[s[i + 1]]++;					// make indegree
 			}
 		}
-		
-		for(int i = 1; i < N + 1; i++) {
-			if(singer[i].size() == 0 || isVisited[i]) continue;		// 다수의 그래프가 존재할 수 있으므로 싹다 돌림
-			topologySort(singer, i);
-		}
-		
-		if(isCycle) {
-			System.out.println(0);
-		}
-		else {
-			StringBuilder sb = new StringBuilder();
-			
-			while(!stack.isEmpty()) {
-				int node = stack.pop();
-				sb.append(node).append(NEW_LINE);
-			}
-			
-			for(int i = 1; i < N + 1; i++) {
-				if(!isVisited[i]) sb.append(i).append(NEW_LINE);
-			}
-			
-			System.out.print(sb);
-		}
+
+		System.out.println(topologySort(N));
 	}
-	
-	private static void topologySort(ArrayList<Integer>[] map, int current) {
-		isVisited[current] = true;
-		
-		for(int next: map[current]) {
-			if(!isVisited[next]) topologySort(map, next);
-			else if(!isFinished[next]) isCycle = true;		// 방문한 정점이 아직 스택에 들어가지 않은경우 -> 사이클
+
+	private static String topologySort(int n){
+		StringBuilder sb = new StringBuilder();
+
+		Queue<Integer> q = new LinkedList<>();
+		Pair[] visit = new Pair[n];
+
+		for(int i = 0; i < n; i++){
+			visit[i] = new Pair(-1, -1);
+			if (indegree[i] != 0) continue;
+
+			q.offer(i);
+			visit[i] = new Pair(i, 1);				// first sequence
+			indegree[i]--;
 		}
-		
-		isFinished[current] = true;
-		stack.push(current);
+
+		while(!q.isEmpty()){
+			int current = q.poll();
+
+			for(int next: seq[current]){
+				indegree[next]--;
+
+				if(visit[next].cnt < visit[current].cnt + 1) {					// find next
+					visit[next] = new Pair(next, visit[current].cnt + 1);
+				}
+
+				if(indegree[next] == 0) q.offer(next);
+			}
+		}
+
+		Arrays.sort(visit);
+		for(int i = 0; i < visit.length; i++){
+			if(visit[i].idx == -1) return "0";				// if she has any seq
+			sb.append(visit[i].idx + 1).append(NEW_LINE);
+		}
+
+		return sb.toString();
 	}
 }
