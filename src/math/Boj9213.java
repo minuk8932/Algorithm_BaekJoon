9a3e63d446e105ad5c39f26cc3fde4c6af0791aa
mibnuk8932 <minuk8932@naver.com>
2020-04-09 19:40:43 +0900

    add Math(Pretty good number) 200409

--- src/math/Boj9213.java
+++ src/math/Boj9213.java
@@ -2,8 +2,6 @@ package math;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -16,8 +14,6 @@ import java.util.StringTokenizer;
  */
 public class Boj9213 {
     private static int[] perfector = new int[1_000_001];
-    private static boolean[] prime = new boolean[1_000_001];
-    private static ArrayList<Integer> primes = new ArrayList<>();
 
     private static final String T = "Test ";
     private static final String C = ": ";
@@ -46,36 +42,11 @@ public class Boj9213 {
     }
 
     private static void init() {
-        Arrays.fill(prime, true);
-        prime[0] = prime[1] = false;
-
-        int loop = (int) (Math.sqrt(prime.length)) + 1;
-
-        for(int i = 2; i < loop; i++) {
-            if(!prime[i]) continue;
-
-            for(int j = i + i; j < prime.length; j += i) {
-                prime[j] = false;
-            }
-        }
-
-        for(int i = 0; i < prime.length; i++){
-            if(prime[i]) primes.add(i);
-        }
-
         for(int i = 2; i < perfector.length; i++) {
-            perfector[i] = 1;
-            int limit = (int) Math.sqrt(i) + 1;
-            if(prime[i]) continue;
+            perfector[i]++;
 
-            for(int p = 2; p < limit; p++) {                // make factor's sum
-                if(i % p != 0) continue;
-
-                int div = i / p;
-                perfector[i] += p;
-                if(div == p) continue;
-
-                perfector[i] += div;
+            for(int j = i + i; j < perfector.length; j += i) {
+                if(j % i == 0) perfector[j] += i;
             }
         }
     }
@@ -83,16 +54,9 @@ public class Boj9213 {
     private static int count(int from, int to, int diff) {
         int count = 0;
 
-        for(int i = from; i <= to; i++){            // find number
-            int target = i - perfector[i];
-
-            if(diff == 0) {
-                if(target == 0) count++;
-            }
-            else {
-                if(target < -diff) continue;
-                if(target <= diff) count++;
-            }
+        for(int i = from; i <= to; i++){                    // find number
+            int target = Math.abs(i - perfector[i]);
+            if(target <= diff) count++;
         }
 
         return count;
