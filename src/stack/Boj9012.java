ffa227d40e8bc1604b12a9aa0a28381c51757519
mibnuk8932 <minuk8932@naver.com>
2020-04-13 19:11:59 +0900

    refactor Stack(Parehthesis) 200413

--- src/stack/Boj9012.java
+++ src/stack/Boj9012.java
@@ -2,6 +2,14 @@ package stack;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ *
+ * @author exponential-e
+ * 백준 9012번: 괄호
+ *
+ * @see https://www.acmicpc.net/problem/9012/
+ *
+ */
 public class Boj9012 {
 	public static final String NEW_LINE = "\n";
 
@@ -10,47 +18,23 @@ public class Boj9012 {
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 
-		while (T > 0) {
-			String VPS = br.readLine();
-			int length = VPS.length();
-			char[] div = new char[length];
-
-			div = VPS.toCharArray();
-			int V = 0, chk = 0;
-			
-			for (int i = 0; i < length; i++) {
-				if (div[i] == '(') {
-					V++;
-				} else {
-					V--;
-				}
-				if(V < 0){
-					chk--;
-				}
-			}
-			
-			if(chk >= 0){
-				if (VPS.startsWith("(") && VPS.endsWith(")")) {
-
-					if (V == 0) {
-						sb.append("YES").append(NEW_LINE);
-					} 
-					else 	{
-						sb.append("NO").append(NEW_LINE);
-					}
-
-				} 
-				else {
-					sb.append("NO").append(NEW_LINE);
-				}
-			}
-			else{
-				sb.append("NO").append(NEW_LINE);
-			}
-
-			T--;
+		while (T-- > 0) {
+			sb.append(judgement(br.readLine().toCharArray()) ? "YES": "NO").append(NEW_LINE);
 		}
+
 		System.out.println(sb.toString());
 	}
 
+	private static boolean judgement(char[] div) {
+		int V = 0;
+
+		for (int i = 0; i < div.length; i++) {
+			if(V < 0) return false;					// not proper
+
+			if (div[i] == '(') V++;
+			else V--;
+		}
+
+		return V == 0;
+	}
 }
