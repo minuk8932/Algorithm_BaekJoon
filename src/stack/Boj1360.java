f5099a19c8e470182bcd3e4adbc8d3d2adb0dfb0
mibnuk8932 <minuk8932@naver.com>
2020-04-16 16:00:33 +0900

    add Stack(Undo) 200416

---
+++ src/stack/Boj1360.java
@@ -0,0 +1,90 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 1360번: 되돌리기
+ *
+ * @see https://www.acmicpc.net/problem/1360/
+ *
+ */
+public class Boj1360 {
+    public static final String UNDO = "undo";
+
+    private static class Query {
+        String cmd;
+        String value;
+        int timer;
+
+        public Query(String cmd, String value, int timer) {
+            this.cmd = cmd;
+            this.value = value;
+            this.timer = timer;
+        }
+    }
+
+    public static void main(String[] args )throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Query> stack = new ArrayDeque<>();
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String c = st.nextToken();
+            String v = st.nextToken();
+            int t = Integer.parseInt(st.nextToken());
+
+            stack.push(new Query(c, v, t));                                 // make reverse query
+        }
+
+        System.out.println(getResult(stack));
+    }
+
+    private static String getResult(ArrayDeque<Query> stack) {
+        ArrayDeque<String> result = new ArrayDeque<>();
+
+        Query start = stack.pop();
+        int undo = -1;
+
+        if(start.cmd.equals(UNDO)) undo = Integer.parseInt(start.value);
+        else result.push(start.value);
+
+        int prev = start.timer;
+
+        while (!stack.isEmpty()) {
+            Query q = stack.pop();
+            int diff = undo - (prev - q.timer);
+
+            if (q.cmd.equals(UNDO)) {
+                int value = Integer.parseInt(q.value);
+
+                if (diff < 0) undo = value;                 // executed
+                else undo = diff;                           // ignored
+            }
+            else {
+                if (diff < 0) {
+                    result.push(q.value);
+                    undo = -1;
+                }
+                else{
+                    undo = diff;
+                }
+            }
+
+            prev = q.timer;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!result.isEmpty()) {
+            sb.append(result.pop());
+        }
+
+        return sb.toString();
+    }
+}
