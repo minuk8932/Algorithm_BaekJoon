ab4fc25bf9a185599e2689f0515f53e789e803df
mibnuk8932 <minuk8932@naver.com>
2020-05-16 16:11:28 +0900

    add Math(Over fitting small) 200516

---
+++ src/math/Boj14604.java
@@ -0,0 +1,99 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 14604번: Over fitting (small)
+ *
+ * @see https://www.acmicpc.net/problem/14604/
+ *
+ */
+public class Boj14604 {
+    private static final String FAN = "LOVELYZ";
+    private static ArrayList<Coordinate> fan = new ArrayList<>();
+    private static ArrayList<Coordinate> notFan = new ArrayList<>();
+
+    private static class Coordinate {
+        int x;
+        int y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if(st.nextToken().equals(FAN)) fan.add(new Coordinate(x, y));
+            else notFan.add(new Coordinate(x, y));
+        }
+
+        System.out.println(Math.max(makeLovelyz(fan, fan), Math.max(makeLovelyz(fan, notFan), makeLovelyz(notFan, notFan))));
+    }
+
+    private static int makeLovelyz(ArrayList<Coordinate> f, ArrayList<Coordinate> nf) {
+        int result = 0;
+
+        int size = f.size();
+        int nSize = nf.size();
+        int[] sizes = {fan.size(), notFan.size()};
+
+        boolean[][] used = new boolean[size][nSize];
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < nSize; j++) {                // make set with fan fan, fan notFan, notFan notFan
+                if(used[i][j]) continue;
+                used[i][j] = true;
+
+                Coordinate current = f.get(i);
+                Coordinate another = nf.get(j);
+
+                if(isSame(current, another)) continue;
+
+                int b = another.x - current.x;                          // get a, b, c
+                int a = current.y - another.y;
+                int c = -a * current.x - b * current.y;
+
+                int[] fans = new int[2];
+                for(int k = 0; k < sizes[0]; k++) {                     // location relation checker
+                    Coordinate target = fan.get(k);
+
+                    if(a * target.x + b * target.y + c >= 0) fans[0]++;
+                    if(a * target.x + b * target.y + c <= 0) fans[1]++;
+                }
+
+                int[] notFans = new int[2];
+                for(int k = 0; k < sizes[1]; k++) {
+                    Coordinate target = notFan.get(k);
+
+                    if(a * target.x + b * target.y + c > 0) notFans[0]++;
+                    if(a * target.x + b * target.y + c < 0) notFans[1]++;
+                }
+
+                if(notFans[0] != 0 && notFans[1] != 0) continue;
+
+                if(notFans[0] == 0) result = Math.max(fans[0], result);
+                if(notFans[1] == 0) result = Math.max(fans[1], result);
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isSame(Coordinate c1, Coordinate c2) {
+        return c1.x == c2.x && c1.y == c2.y;
+    }
+}
