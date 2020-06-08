f1349f6d576e8c9513b460e7887ad196ec7d6625
mibnuk8932 <minuk8932@naver.com>
2020-06-08 16:46:32 +0900

    add NetworkFlow(N-Rook) 200608

---
+++ src/network_flow/Boj1760.java
@@ -0,0 +1,133 @@
+package network_flow;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 1760번: N - Rook
+ *
+ * @see https://www.acmicpc.net/problem/1760
+ *
+ */
+public class Boj1760 {
+    private static int[][] row, col, board, connected;
+    private static int N, M, R, C;
+
+    private static int[] aMatch, bMatch;
+    private static int[] visit;
+
+    private static int count;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        row = new int[N][M];
+        col = new int[N][M];
+
+        int size = N * M / 2;
+        connected = new int[size + 1][size + 1];
+        visit = new int[size + 1];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        col = modeling(true);           // right side
+        row = modeling(false);          // left side
+
+        connection();                        // setting capacity
+        System.out.println(bipartiteMath());
+    }
+
+    private static int bipartiteMath() {
+        aMatch = new int[N];
+        bMatch = new int[M];
+        Arrays.fill(aMatch, -1);
+        Arrays.fill(bMatch, -1);
+
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            count++;
+            result += recursion(i);
+        }
+
+        return result;
+    }
+
+    private static int recursion(int current) {
+        if (visit[current] == count) return 0;
+        visit[current] = count;
+
+        for(int next = 0; next < M; next++) {
+            if(connected[current][next] == 0) continue;
+
+            if(bMatch[next] == -1 || recursion(bMatch[next]) == 1) {
+                aMatch[current] = next;
+                bMatch[next] = current;
+
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private static void connection() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] != 0) continue;
+                connected[row[i][j]][col[i][j]] = 1;    // from -> to, capacity
+
+                R = Math.max(R, row[i][j]);             // resize
+                C = Math.max(C, col[i][j]);
+            }
+        }
+
+        N = R + 1;
+        M = C + 1;
+    }
+
+    private static int[][] modeling(boolean flag) {
+        int[][] arr = new int[N][M];
+
+        int outer = flag ? N: M;
+        int inner = flag ? M: N;
+        int value = 0;
+
+        for (int i = 0; i < outer; i++) {
+            boolean used = false;
+
+            for (int j = 0; j < inner; j++) {
+                int[] index = {flag ? i: j, flag ? j: i};
+
+                if (board[index[0]][index[1]] == 0 || board[index[0]][index[1]] == 1) {
+                    arr[index[0]][index[1]] = value;
+                    used = true;
+                }
+                else {
+                    if(!used) continue;
+
+                    value++;
+                    used = false;
+                }
+            }
+
+            if (used) value++;
+        }
+
+        return arr;
+    }
+}
