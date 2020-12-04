a1dfcbbab66552cbf356df647a5722104b18c74d
minuk8932 <minuk8932@naver.com>
2020-12-04 20:27:42 +0900

    add MST(Copy robots) 201204

--- src/minimumcost_spanning_tree/Boj1944.java
+++ src/minimumcost_spanning_tree/Boj1944.java
@@ -1,10 +1,7 @@
 package minimumcost_spanning_tree;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 
@@ -17,58 +14,33 @@ import java.util.Queue;
 public class Boj1944 {
 	private static PriorityQueue<Node> pq = new PriorityQueue<>();
 	private static int[] parent;
+	private static int N, M;
 	
 	private static final int[][] DIRECTIONS = {{1, 0}, {0, 1}, {-1 ,0}, {0, -1}};
 	private static final int ROW = 0;
 	private static final int COL = 1;
-	
-	public static void main(String[] args) throws Exception{
-		InputReader in = new InputReader(System.in);
-		int N = in.readInt();
-		int M = in.readInt();
-		
-		char[][] maze = new char[N][N];
-		int[][] key = new int[N][N];
-		int value = 0;
-		
-		for(int i = 0; i < N; i++) {
-			String line = in.readString();
-			
-			for(int j = 0; j < N; j++) {
-				maze[i][j] = line.charAt(j);
-				
-				if(maze[i][j] == 'S' || maze[i][j] == 'K') key[i][j] = value++;		// 노드 설정
-			}
-		}
+	private static final char BLOCK = '1';
+	private static final char START = 'S';
+	private static final char KEY = 'K';
 
-		boolean search = bfs(maze, N, key);
-		
-		if(search) {
-			parent = new int[M + 1];
-			init(M);
-			
-			System.out.println(kruskal(pq));
-		}
-		else {
-			System.out.println(-1);
-		}
-	}
-	
+	private static ArrayList<Point> start = new ArrayList<>();
+	private static int head = -1;
+
 	private static class Point {
 		int row;
 		int col;
-		
+
 		public Point(int row, int col) {
 			this.row = row;
 			this.col = col;
 		}
 	}
-	
+
 	private static class Node implements Comparable<Node>{
 		int from;
 		int to;
 		int cost;
-		
+
 		public Node(int from, int to, int cost) {
 			this.from = from;
 			this.to = to;
@@ -81,87 +53,110 @@ public class Boj1944 {
 		}
 	}
 	
-	private static boolean bfs(char[][] map, int n, int[][] k) {		
-		for(int row = 0; row < n; row++) {
-			for(int col = 0; col < n; col++) {
-				if(map[row][col] != 'S' && map[row][col] != 'K') continue;
-				boolean isReached = false;
-				
-				Queue<Point> q = new LinkedList<>();
-				q.offer(new Point(row, col));
-				
-				int[][] isVisited = new int[n][n];
-				isVisited[row][col] = 1;
-				
-				while(!q.isEmpty()) {
-					Point current = q.poll();
-					
-					for(final int[] DIRECTION: DIRECTIONS) {
-						int nextRow = current.row + DIRECTION[ROW];
-						int nextCol = current.col + DIRECTION[COL];
-						
-						if(nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n) {
-							if(isVisited[nextRow][nextCol] == 0 && map[nextRow][nextCol] != '1') {
-								isVisited[nextRow][nextCol] = isVisited[current.row][current.col] + 1;
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		N = in.readInt();
+		M = in.readInt();
+		
+		char[][] maze = new char[N][N];
+		int[][] key = new int[N][N];
+		int value = 0;
+		
+		for(int i = 0; i < N; i++) {
+			String line = in.readString();
+			
+			for(int j = 0; j < N; j++) {
+				maze[i][j] = line.charAt(j);
 
-								if(map[nextRow][nextCol] == 'S' || map[nextRow][nextCol] == 'K') {			// 도달 가능
-									pq.offer(new Node(k[row][col], k[nextRow][nextCol], isVisited[nextRow][nextCol] - 1));
-									isReached = true;
-								}
-									
-								q.offer(new Point(nextRow, nextCol));
-							}
-						}
-					}
+				if(maze[i][j] == KEY || maze[i][j] == START){
+					if (maze[i][j] == START) head = value;
+					key[i][j] = value++;
+					start.add(new Point(i, j));
 				}
-				
-				if(!isReached) return false;		// 도달 못하는 경우
 			}
 		}
-		
-		return true;
+
+		bfs(maze, key);
+		int cost = kruskal(pq);
+		System.out.println(-parent[find(head)] == M + 1 ? cost: -1);		// find all keies?
 	}
 	
-	private static void init(int n) {
-		for(int i = 0; i < n + 1; i++) {
-			parent[i] = i;
+	private static void bfs(char[][] map, int[][] k) {
+		for(Point s: start) {
+			Queue<Point> q = new LinkedList<>();
+			q.offer(s);
+
+			int[][] isVisited = new int[N][N];
+			isVisited[s.row][s.col] = 1;
+
+			while (!q.isEmpty()) {
+				Point current = q.poll();
+
+				for (final int[] DIRECTION : DIRECTIONS) {
+					int nextRow = current.row + DIRECTION[ROW];
+					int nextCol = current.col + DIRECTION[COL];
+
+					if (outOfRange(nextRow, nextCol)) continue;
+					if (isVisited[nextRow][nextCol] != 0 || map[nextRow][nextCol] == BLOCK) continue;
+					isVisited[nextRow][nextCol] = isVisited[current.row][current.col] + 1;
+
+					if (map[nextRow][nextCol] == KEY || map[nextRow][nextCol] == START) {				// cost save
+						pq.offer(new Node(k[s.row][s.col], k[nextRow][nextCol], isVisited[nextRow][nextCol] - 1));
+					}
+
+					q.offer(new Point(nextRow, nextCol));
+				}
+			}
+		}
+	}
+	
+	private static void init() {
+		for(int i = 0; i <= M; i++) {
+			parent[i] = -1;
 		}
 	}
 	
 	private static int find(int x) {
-		if(x == parent[x]) return x;
-		else return find(parent[x]);
+		if(parent[x] < 0) return x;
+		else return parent[x] = find(parent[x]);
 	}
 	
-	private static void merge(int x, int y) {
+	private static boolean merged(int x, int y) {
 		x = find(x);
 		y = find(y);
-		
-		if(x < y) parent[y] = x;
-		else parent[x] = y;
-	}
-	
-	private static boolean isCycle(int x, int y) {
-		x = find(x);
-		y = find(y);
-		
+
 		if(x == y) return true;
-		else return false;
+
+		if(parent[x] < parent[y]){
+			parent[x] += parent[y];
+			parent[y] = x;
+		}
+		else{
+			parent[y] += parent[x];
+			parent[x] = y;
+		}
+
+		return false;
 	}
 	
 	private static int kruskal(PriorityQueue<Node> pq) {
+		parent = new int[M + 1];
+		init();
+
 		int minCost = 0;
 		
 		while(!pq.isEmpty()) {
 			Node next = pq.poll();
 			
-			if(!isCycle(next.from, next.to)) {
-				merge(next.from, next.to);
-				minCost += next.cost;
-			}
+			if(merged(next.from, next.to)) continue;
+			minCost += next.cost;
 		}
 		
 		return minCost;
+	}
+
+	private static boolean outOfRange(int row, int col) {
+		return row < 0 || row >= N || col < 0 || col >= N;
 	}
 	
 	private static class InputReader {
