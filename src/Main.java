761ca705140ceac598dd879f4fef32c69b0390ee
minuk8932 <minuk8932@naver.com>
2021-03-08 16:04:27 +0900

    refactory DP(01 tile) 210308

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
+
+    private static final int MOD = 15_746;
+    private static int[] dp;
+
     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new int[N + 1];
+        Arrays.fill(dp, -1);
+        System.out.println(recursion(N));
+    }
+
+    private static int recursion(int n) {
+        if(n == 0) return 1;
+        if(dp[n] != -1) return dp[n];
+
+        int result = recursion(n - 1);
+
+        if(n >= 2) result = modulation(result, recursion(n - 2));
+        return dp[n] = result;
+    }
+
+    private static int modulation(int a, int b) {
+        return ((a % MOD) + (b % MOD)) % MOD;
     }
 }
--- src/dynamic_programming/Boj1904.java
+++ src/dynamic_programming/Boj1904.java
@@ -1,33 +1,50 @@
 package dynamic_programming;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
+/**
+ *
+ * @author exponential-e
+ * 백준 1904번: 01 타일
+ *
+ * @see https://www.acmicpc.net/problem/1904
+ *
+ */
 public class Boj1904 {
-	private static final int SPARE = 15746;
-	
-	public static void main(String[] args) throws Exception{
+
+	private static final int MOD = 15_746;
+	private static int[] dp;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long[] tile = new long[N + 1];
-		
-		if(N==1){
-			tile[1] = 1;
-		}
-		
-		if(N==2){
-			tile[2] = 2;
-		}
-		
-		if(N > 2){
-			tile[1] = 1;
-			tile[2] = 2;
-			
-			for(int i=3; i < N+1; i++){
-				tile[i] = (tile[i - 1] + tile[i - 2]) % SPARE;
-			}
-		}
-		
-		
-		System.out.println(tile[N]);
+
+		dp = new int[N + 1];
+		Arrays.fill(dp, -1);
+		System.out.println(recursion(N));
+	}
+
+	/**
+	 *
+	 * fibonacci
+	 *
+	 * @param n
+	 * @return memoization
+	 */
+	private static int recursion(int n) {
+		if(n == 0) return 1;
+		if(dp[n] != -1) return dp[n];
+
+		int result = recursion(n - 1);
+
+		if(n >= 2) result = modulation(result, recursion(n - 2));
+		return dp[n] = result;
+	}
+
+	private static int modulation(int a, int b) {
+		return ((a % MOD) + (b % MOD)) % MOD;
 	}
 }
+
