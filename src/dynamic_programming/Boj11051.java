ae9dc5e272df708cffb835991ce7225eacd26e88
minuk8932 <minuk8932@naver.com>
2021-03-09 15:10:24 +0900

    refactory DP(Combination) 210309

--- src/dynamic_programming/Boj11051.java
+++ src/dynamic_programming/Boj11051.java
@@ -1,36 +1,59 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * 
- * 	@author minchoba
+ * 	@author exponential-e
  *	백준 11051번: 이항계수 2
  *
  *	@see https://www.acmicpc.net/problem/11052/
  *
  */
 public class Boj11051 {
+
 	private static final int MOD = 10_007;
-	private static final int INF = 1_001;
-	
-	private static long[][] dp = new long[INF][INF];
-	
-	public static void main(String[] args) throws Exception{
-		// 버퍼를 통한 값 입력
+	private static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
-		
-		System.out.println(combine(N, K) % MOD);	// 조합 메소드를 통한 이항계수 구하기
+
+		dp = new int[N + 1][K + 1];
+		for(int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+
+		System.out.println(combination(N, K));
 	}
-	
-	private static long combine(int n, int k) {
-		if(k == 0 || n == k) return 1;			// 0, 1 의경우 조합의 정의에 따라 1
-		if(dp[n][k] > 0) return dp[n][k] % MOD;	// dp 배열에 값이 존재하는 경우
-		
-		return dp[n][k] = (combine(n-1, k-1) + combine(n-1, k)) % MOD; // 처음 계산하는 조합인 경우 페르마 소정리 이용
+
+	/**
+	 *
+	 * Combination
+	 *
+	 * line 52: nCr = n-1Cr + n-1Cr-1
+	 *
+	 * @param n
+	 * @param k
+	 * @return
+	 */
+	private static int combination(int n, int k) {
+		if(n == k || k == 0) return 1;
+		if(k == 1) return n;
+		if(dp[n][k] != -1) return dp[n][k];
+
+		int result = 0;
+		if(k > (n >> 1)) k = n - k;
+
+		result = modulation(result, modulation(combination(n - 1, k), combination(n - 1, k - 1)));
+		return dp[n][k] = result;
+	}
+
+	private static int modulation(int a, int b) {
+		return ((a % MOD) + (b % MOD)) % MOD;
 	}
 }
