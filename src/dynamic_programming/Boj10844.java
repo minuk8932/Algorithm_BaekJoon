1ccd76d496664284e3ff3d67fb901fb4ac097f77
minuk8932 <minuk8932@naver.com>
2021-03-09 14:21:59 +0900

    refactory DP(Easy stair number) 210309

--- src/dynamic_programming/Boj10844.java
+++ src/dynamic_programming/Boj10844.java
@@ -1,52 +1,62 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * 
- * 	@author minchoba
+ * 	@author exponential-e
  *	백준 10844번 : 쉬운계단수
  *
  *	@see https://www.acmicpc.net/problem/10844/
  *
  */
 public class Boj10844 {
-	private static final int DIV = 1_000_000_000;
-	private static final int NUMS = 10;
-	
-	public static void main(String[] args) throws Exception{
-		// 버퍼를 통한 값 입력
+	private static final int MOD = 1_000_000_000;
+	private static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		
-		long[][] dp = new long[N + 1][NUMS];	// 인접한 숫자의 두 차이에의해 발생, 그리고 각 숫자의 범위는 0~9까지
-		
-		for(int i = 1; i < NUMS; i++){
-			dp[1][i] = 1;
+
+		dp = new int[N + 1][11];
+		for(int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		
-		for(int i = 2; i < N + 1; i++){
-			for(int j = 0; j < NUMS; j++){		// 각 경우의 수별로 값들을 임시 변수에 담고, 나머지값을 구해 dp 배열에 할당
-				long tmp = 0;
-				
-				if(j + 1 <= 9){
-					tmp += dp[i - 1][j + 1];
-				}
-				
-				if(j - 1 >= 0){
-					tmp += dp[i - 1][j - 1];
-				}
-				
-				dp[i][j] = tmp % DIV;
+
+		System.out.println(recursion(N, -1));
+	}
+
+	/**
+	 *
+	 * recursion
+	 *
+	 * line 46 ~ 54: first value fix by 1 ~ 9 & next cases.
+	 *
+	 * @param n
+	 * @param prev
+	 * @return memoization
+	 */
+	private static int recursion(int n, int prev) {
+		if(n == 0) return 1;
+		if(dp[n][prev == -1 ? 10: prev] != -1) return dp[n][prev];
+
+		int result = 0;
+
+		if(prev == -1) {
+			for(int i = 1; i < 10; i++) {
+				result = modulation(result, recursion(n - 1, i));
 			}
 		}
-		
-		long res = 0;
-		
-		for(int i = 0; i < NUMS; i++){
-			res += dp[N][i] % DIV;
+		else {
+			if(prev != 9) result = modulation(result, recursion(n - 1, prev + 1));
+			if(prev != 0) result = modulation(result, recursion(n - 1, prev - 1));
 		}
-		
-		System.out.println(res % DIV);		// 결과값 출력
+
+		return dp[n][prev == -1 ? 10: prev] = result;
+	}
+
+	private static int modulation(int a, int b) {
+		return ((a % MOD) + (b % MOD)) % MOD;
 	}
 }
