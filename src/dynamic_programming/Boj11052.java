9c019aac41eba5752164c7adca22c04afa909d38
minuk8932 <minuk8932@naver.com>
2021-03-09 13:52:58 +0900

    refactory DP(Buy cards) 210309

--- src/dynamic_programming/Boj11052.java
+++ src/dynamic_programming/Boj11052.java
@@ -1,6 +1,7 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -12,29 +13,47 @@ import java.util.StringTokenizer;
  * 
  */
 public class Boj11052 {
-	private static final String SPACE = " ";
+
+	private static int[] dp;
+	private static int[] pack;
+	private static int N;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-		int N = Integer.parseInt(br.readLine());
-		int[] fish = new int[N + 1];
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
-		for (int i = 1; i < N + 1; i++) {
-			fish[i] = Integer.parseInt(st.nextToken());
+		pack = new int[N + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			pack[i] = Integer.parseInt(st.nextToken());
 		}
-		br.close();
-		
-		int[] dp = new int[N + 1]; // [fish size]
 
-		for(int i = 1; i <= N ; i++){
-			for(int j = 1; j <= N; j++){
-				if(j >= i){
-					dp[j] = Math.max(dp[j], dp[j-i] + fish[i]);
-				}
-			}
-		}
-		System.out.println(dp[N]);
+		dp = new int[N + 1];
+		Arrays.fill(dp, -1);
+
+		System.out.println(recursion(N));
 	}
+
+	/**
+	 *
+	 * recursion
+	 *
+	 * line 53: add next cards & current pack[i]
+	 *
+	 * @param cards
+	 * @return memoization
+	 */
+	private static int recursion(int cards) {
+		if(cards == 0) return 0;
+
+		if(dp[cards] != -1) return dp[cards];
+		int result = -1;
+
+		for(int i = 1; i <= cards; i++) {
+			result = Math.max(result, recursion(cards - i) + pack[i]);
+		}
+
+		return dp[cards] = result;
+	}
+}
 }
