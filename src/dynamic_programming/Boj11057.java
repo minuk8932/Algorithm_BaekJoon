885bfe935513f8d9545fff07593aba69695938db
minuk8932 <minuk8932@naver.com>
2021-03-09 14:46:42 +0900

    refactory DP(Increment number) 210309

--- src/dynamic_programming/Boj11057.java
+++ src/dynamic_programming/Boj11057.java
@@ -1,41 +1,61 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
+/**
+ *
+ * @author exponential-e
+ * 백준 11057번: 오르막 수
+ *
+ * @see https://www.acmicpc.net/problem/11057
+ *
+ */
 public class Boj11057 {
-	private static final long MOD = 10_007;
-	
-	public static void main(String[] args) throws Exception{
+
+	private static final int MOD = 10_007;
+	private static int[][] dp;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long[][] dp = new long[N+1][10];		// [자릿수][앞자리 숫자]
-		
-		dp[0][9] = 1;
-		
-		for(int i = 0; i < 10; i++){
-			if(i == 0){
-				dp[1][i] = dp[0][9];
-			}
-			else{
-				dp[1][i] = dp[1][i - 1] + 1;
-			}
+
+		dp = new int[N + 1][11];
+		for(int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		
-		for(int i = 2; i < N+1; i++){
-			for(int j = 0; j < 10; j++){
-				if(j == 0){
-					dp[i][j] = dp[i - 1][9] % MOD;
-				}
-				else{
-					dp[i][j] = (dp[i][j - 1] + (dp[i][0] - dp[i - 1][j - 1]))% MOD;
-					
-					if(dp[i][j] <= MOD){				// 1) Modulation 도중 위의 식으로 인한 음수 발생 시, 상수 MOD를 더해 양수로 바꿔줌
-						dp[i][j] = MOD + dp[i][j];
-					}
-				}
-			}
+
+		System.out.println(recursion(N, -1));
+	}
+
+	/**
+	 *
+	 * recursion
+	 *
+	 * line 47 ~ 49: distinct each ciphers, 1 & others
+	 *
+	 * @param n
+	 * @param prev
+	 * @return memoization
+	 */
+	private static int recursion(int n, int prev) {
+		if(n == 0) return 1;
+		if(dp[n][idx(prev)] != -1) return dp[n][idx(prev)];
+
+		int result = 0;
+
+		for(int cur = idx(prev) % 10; cur < 10; cur++) {
+			result = modulation(result, recursion(n - 1, cur));
 		}
-		
-		System.out.println(dp[N][9] % MOD);		// 1)에서 최종 값에 MOD가 더해지면 상수 10007보다 큰 수가 나오므로 다시한번 Modulation
+
+		return dp[n][idx(prev)] = result;
+	}
+
+	private static int idx(int p) {
+		return p == -1 ? 10: p;
+	}
+
+	private static int modulation(int a, int b) {
+		return ((a % MOD) + (b % MOD)) % MOD;
 	}
 }
