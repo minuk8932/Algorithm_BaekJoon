c00bb8f75fc4e51de8493e880e8aeb83fe3a3daa
minuk8932 <minuk8932@naver.com>
2021-03-09 18:14:37 +0900

    refactory DP(The biggest increment sequence) 210309

--- src/dynamic_programming/Boj11055.java
+++ src/dynamic_programming/Boj11055.java
@@ -1,11 +1,12 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * 
- * @author minchoba
+ * @author exponential-e
  *	백준 11055번 : 가장 큰 증가하는 부분 수열
  *
  *	@see https://www.acmicpc.net/problem/11055
@@ -13,40 +14,47 @@ import java.util.StringTokenizer;
  */
 
 public class Boj11055 {
-	private static final String SPACE = " ";
-	
-	public static void main(String[] args) throws Exception{
-		//버퍼를 통한 값 입력
+
+	private static int[][] dp;
+	private static int[] seq;
+	private static int N;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] A = new int[N + 1];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), SPACE);
-		for(int i = 1; i < N + 1; i++){
-			A[i] = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+
+		seq = new int[N + 1];
+		dp = new int[N + 1][1_002];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int[] dp = new int[N + 1];
-		
-		dp[1] = A[1];
-		
-		for(int i = 1; i < N + 1; i++){
-			for(int j = 1; j < i + 1; j++){
-				if(A[i] > A[j - 1]){
-					dp[i] = Math.max(dp[i], dp[j - 1] + A[i]);		/** 
-																					* 	@see Boj11053 : 해당 문제와 풀이는 유사함
-																					* 		차이점은 위의 문제는 증가 할 때 마다 1씩 더해서 dp 배열에 저장했지만,
-																					* 		이 문제의 핵심은 해당 인덱스 안의 값을 합하여 그 최대 값을 찾는 것.
-																					*/
-				}
-			}
+
+		for(int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		
-		int max = 0;
-		for(int i = 1; i < N + 1; i++){
-			max = Math.max(dp[i], max);							// 각 자리별로 부분 수열의 합 중 가장 큰 값을 max에 저장
-		}
-		
-		System.out.println(max);									// 결과 출력
+
+		System.out.println(recursion(N, 1_001));
+	}
+
+	/**
+	 *
+	 * Recursion
+	 *
+	 * line 55: not add next
+	 * line 56: add next
+	 *
+	 * @param n
+	 * @param prev
+	 * @return
+	 */
+	private static int recursion(int n, int prev) {
+		if(n < 0) return 0;
+		if(dp[n][prev] != -1) return dp[n][prev];
+
+		int result = recursion(n - 1, prev);
+		if(prev == -1 || prev > seq[n]) result = Math.max(result, recursion(n - 1, seq[n]) + seq[n]);
+
+		return dp[n][prev] = result;
 	}
 }
