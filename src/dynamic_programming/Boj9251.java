8fedf0ab7d9561eb95b55b57b17110f110028c62
minuk8932 <minuk8932@naver.com>
2021-03-11 12:39:23 +0900

    refactory DP(LCS) 210311

--- src/dynamic_programming/Boj9251.java
+++ src/dynamic_programming/Boj9251.java
@@ -5,54 +5,54 @@ import java.util.Arrays;
 
 /**
  * 
- * @author minchoba
- *		Boj 9251번 : LCS 문제
- *					최장 공통 부분 알고리즘 (Longest Common Subsequence)
+ * @author exponential-e
+ * 백준 9251번 : LCS
+ *
+ * @see https://www.acmicpc.net/problem/9251
  *
  */
 public class Boj9251 {
+
+	private static char[] src, snk;
+	private static int[][] dp;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String stand = br.readLine();
-		String comp = br.readLine();
+		src = br.readLine().toCharArray();
+		snk = br.readLine().toCharArray();
 
-		int leng2 = comp.length();
-		int leng1 = stand.length();
-		int[][] dp = new int[leng2 + 1][leng1 + 1];
-		
-		// dp를 통한 최장 공통부분 수치 계산 :  1열의 경우
-		for(int i = 1; i < leng1 + 1; i++){
-			if(comp.charAt(0) == stand.charAt(i - 1)){			// 해당 비교 단어가 서로 같다면 좌측 상단의 dp값 + 1
-				dp[1][i] = dp[0][i - 1] + 1;
-			}
-			else{
-				dp[1][i] = Math.max(dp[0][i], dp[1][i-1]);				// 같지 않다면, 바로 왼쪽의 dp 값과 바로 위의 dp값의 최대 값을 현재 배열에 할당
-			}
+		dp = new int[src.length + 1][snk.length + 1];
+		for(int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
 		}
-		
-		// 배열 전체에 값 할당
-		for(int i = 2; i < leng2 + 1; i++){
-			for(int j = 1; j < leng1 + 1; j++){
-				if(comp.charAt(i - 1) == stand.charAt(j - 1)){
-					dp[i][j] = dp[i - 1][j - 1] + 1;
-				}
-				else{
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j-1]);
-				}
-			}
+
+		System.out.println(recursion(src.length, snk.length));
+	}
+
+	/**
+	 *
+	 * Recursion
+	 *
+	 * line 50: src[N] != snk[M], then we need check two cases by decrease N or M
+	 * line 53: src[N] == snk[M], then we check just one case, next N & next M
+	 *
+	 * @param N
+	 * @param M
+	 * @return
+	 */
+	private static int recursion(int N, int M) {
+		if(N == 0 || M == 0) return 0;
+
+		int result = dp[N][M];
+		if(result != -1) return result;
+
+		if(src[N - 1] != snk[M - 1]) {
+			result = Math.max(recursion(N, M - 1), recursion(N - 1, M));
 		}
-		
-		int max = 0, cnt = 0;
-		
-		// 최장 공통부분의 길이 측정
-		for(int i = 1; i < leng2 + 1; i++){
-			for(int j = 1; j < leng1 + 1; j++){
-				if(max < dp[i][j] && dp[i][j] > dp[i][j - 1]){
-					max = Math.max(max, dp[i][j]);
-					cnt++;
-				}
-			}
+		else {
+			result = recursion(N - 1, M - 1) + 1;
 		}
-		System.out.println(cnt);
+
+		return dp[N][M] = result;
 	}
 }
