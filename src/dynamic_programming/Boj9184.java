90e09142ab935102738889f1424e34af89be99d6
minuk8932 <minuk8932@naver.com>
2021-03-21 17:01:22 +0900

    refactory DP(Funny function execution) 210321

--- src/dynamic_programming/Boj9184.java
+++ src/dynamic_programming/Boj9184.java
@@ -1,6 +1,7 @@
 package dynamic_programming;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -12,66 +13,74 @@ import java.util.StringTokenizer;
  *
  */
 public class Boj9184 {
-	private static final String TERMINATE = "-1 -1 -1";
-	private static final String NEW_LINE = "\n";
-	
-	private static final String FUNCTION_EXPRESSION_OPEN = "w(";
+	private static long[][][] dp = new long[21][21][21];
+
+	private static final String FUNC_PREV = "w(";
+	private static final String FUNC_POST = ") = ";
 	private static final String COMMA = ", ";
-	private static final String FUNCTION_EXPRESSION_CLOSE = ") = ";
-	
+	private static final String NEW_LINE = "\n";
+
 	public static void main(String[] args) throws Exception{
-		// 버퍼를 통한 값 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		int[][][] dp = new int[21][21][21];
-		
-		for(int i = 0; i < dp.length; i++) {
-			for(int j = 0; j < dp.length; j++) {
-				for(int k = 0; k < dp.length; k++) {				// 조건에 따라 0이하의 값이 들어온 경우 dp 배열에 1 저장
-					if(i < 1 || j < 1 || k < 1) dp[i][j][k] = 1;
-				}
-			}
-		}
-		
-		for(int i = 1; i < dp.length; i++) {				// 1 이상 20 이하의 수가 들어온 경우의 값들을 배열에 저장
-			for(int j = 1; j < dp.length; j++) {
-				for(int k = 1; k < dp.length; k++) {
-					if(i < j && j < k) dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
-					else dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1];
-				}
-			}
-		}
-		
+
+		init();
+
 		while(true) {
-			String cmd = br.readLine();
-			if(TERMINATE.equals(cmd)) break;			// 입력으로 종료 문자열이 들어온 경우
-			
-			StringTokenizer st = new StringTokenizer(cmd);
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			
-			if(a < 1 || b < 1 || c < 1) {				// 적어도 1개가 1미만의 수인 경우 1을 버퍼에 저장
-				sb.append(FUNCTION_EXPRESSION_OPEN)
-				.append(a).append(COMMA).append(b).append(COMMA).append(c)
-				.append(FUNCTION_EXPRESSION_CLOSE)
-				.append(1).append(NEW_LINE);
-			}
-			else if(a > 20 || b > 20 || c > 20) {		// 적어도 1개가 20보다 큰 수인 경우 dp[20][20][20]을 버퍼에 저장
-				sb.append(FUNCTION_EXPRESSION_OPEN)
-				.append(a).append(COMMA).append(b).append(COMMA).append(c)
-				.append(FUNCTION_EXPRESSION_CLOSE)
-				.append(dp[20][20][20]).append(NEW_LINE);
-			}
-			else {										// 그 외의 수들은 배열의 인덱스로 참조해 값을 버퍼에 저장
-				sb.append(FUNCTION_EXPRESSION_OPEN)
-				.append(a).append(COMMA).append(b).append(COMMA).append(c)
-				.append(FUNCTION_EXPRESSION_CLOSE)
-				.append(dp[a][b][c]).append(NEW_LINE);
+
+			if(a == -1 && b == -1 && c == -1) break;
+
+			sb.append(FUNC_PREV).append(a).append(COMMA).append(b).append(COMMA).append(c).append(FUNC_POST)
+					.append(underValue(a, b, c) ? 1: overValue(a, b, c) ? dp[20][20][20]: dp[a][b][c]).append(NEW_LINE);
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	private static boolean overValue(int a, int b, int c) {
+		return a > 20 || b > 20 || c > 20;
+	}
+
+	private static long recursion(int a, int b, int c) {
+		if(a <= 0 || b <= 0 || c <= 0) return 1;
+
+		if(dp[a][b][c] != -1) return dp[a][b][c];
+		long result;
+
+		if(a < b && b < c) result = recursion(a, b, c - 1) + recursion(a, b - 1, c - 1) - recursion(a, b - 1, c);
+		else result = recursion(a - 1, b, c) + recursion(a - 1, b - 1, c) + recursion(a - 1, b, c - 1) - recursion(a - 1, b - 1, c - 1);
+
+		return dp[a][b][c] = result;
+	}
+
+	private static boolean underValue(int a, int b, int c) {
+		return a <= 0 || b <= 0 || c <= 0;
+	}
+
+	/**
+	 *
+	 * init
+	 *
+	 * line 78 ~ 84: find all cases
+	 *
+	 */
+	private static void init() {
+		for(int i = 0; i <= 20; i++) {
+			for(int j = 0; j <= 20; j++) {
+				Arrays.fill(dp[i][j], -1);
 			}
 		}
-		
-		System.out.println(sb);		// 결과 값 한번에 출력
+
+		for(int a = 20; a >= 0; a--) {
+			for(int b = 20; b >= 0; b--) {
+				for(int c = 20; c >= 0; c--) {
+					recursion(a, b, c);
+				}
+			}
+		}
 	}
 }
