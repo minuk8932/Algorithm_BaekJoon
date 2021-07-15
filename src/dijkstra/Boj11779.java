d26285e2845bc07886af2ee649a9165366bd2bfa
minuk8932 <minuk8932@naver.com>
2021-07-15 13:40:12 +0900

    refactor Dijkstra(Find the least cost 2) 210715

--- src/dijkstra/Boj11779.java
+++ src/dijkstra/Boj11779.java
@@ -1,10 +1,7 @@
 package dijkstra;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * 
@@ -21,59 +18,9 @@ public class Boj11779 {
 	private static long[] cost = null;
 	private static int[] trace = null;
 
+	private static final long INF = 1_000_000_000_000L;
 	private static final String SPACE = " ";
 	private static final String NEW_LINE = "\n";
-
-	public static void main(String[] args) throws Exception {
-		// 버퍼를 통한 값 입력
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		
-		map = new ArrayList[n + 1];
-		trace = new int[n + 1];
-		cost = new long[n + 1];
-		
-		for (int i = 0; i < n + 1; i++) {
-			map[i] = new ArrayList<>();
-			trace[i] = -1;
-			cost[i] = Long.MAX_VALUE;
-		}
-
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			
-			map[from].add(new Node(to, cost));
-		}
-
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		
-		dijkstra(start, end);		// 다익스트라 알고리즘 메소드
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(cost[end]).append(NEW_LINE);		// 목적지까지의 최소비용을 버퍼에 저장
-		
-		LinkedList<Integer> path = new LinkedList<>();
-		path.add(end);
-		while(trace[end] != start) {		// 시작점 전까지 도달가능한 노드의 번호를 연결리스트에 저장
-			end = trace[end];
-			path.add(end);
-		}
-		path.add(start);		// 시작점도 저장
-		
-		sb.append(path.size()).append(NEW_LINE);		// 경로의 갯수를 버퍼에 저장
-
-		while(!path.isEmpty()) {
-			sb.append(path.removeLast()).append(SPACE);		// 연결리스트 역순으로 뽑아내어 버퍼에 경로를 저장
-		}
-		
-		System.out.print(sb.toString());				// 결과 값 한번에 출력
-	}
 
 	private static class Node implements Comparable<Node> {
 		int node;
@@ -92,24 +39,97 @@ public class Boj11779 {
 		}
 	}
 
-	private static void dijkstra(int s, int e) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		
+		map = new ArrayList[n];
+		trace = new int[n];
+		cost = new long[n];
+		
+		for (int i = 0; i < n; i++) {
+			map[i] = new ArrayList<>();
+			trace[i] = -1;
+			cost[i] = INF;
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			int cost = Integer.parseInt(st.nextToken());
+			
+			map[from].add(new Node(to, cost));
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken()) - 1;
+		int end = Integer.parseInt(st.nextToken()) - 1;
+
+		dijkstra(start);
+		System.out.print(printPath(start, end));
+	}
+
+	/**
+	 *
+	 * Print path
+	 *
+	 * line 91 ~ 94: tracking the shortest path
+	 *
+	 * @param s
+	 * @param e
+	 * @return
+	 */
+	private static String printPath(int s, int e) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(cost[e]).append(NEW_LINE);
+
+		Deque<Integer> path = new ArrayDeque<>();
+		path.add(e);
+
+		while(trace[e] != s) {
+			e = trace[e];
+			path.offer(e);
+		}
+
+		path.offer(s);
+		sb.append(path.size()).append(NEW_LINE);
+
+		while(!path.isEmpty()) {
+			sb.append(path.pollLast() + 1).append(SPACE);
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 *
+	 * Dijkstra
+	 *
+	 * line 123: pre-cutting by DP
+	 * line 130: save path
+	 *
+	 * @param s
+	 */
+	private static void dijkstra(int s) {
 		cost[s] = 0;
 		
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.offer(s);
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(s, 0));
 
 		while (!pq.isEmpty()) {
-			int current = pq.poll();
+			Node current = pq.poll();
+			if(current.val > cost[current.node]) continue;
 
-			for (Node next : map[current]) {
-				long nextCost = next.val + cost[current];
-				
-				if (cost[next.node] > nextCost) {
-					cost[next.node] = nextCost;
-					trace[next.node] = current;
+			for (Node next : map[current.node]) {
+				long nextCost = next.val + cost[current.node];
+
+				if (cost[next.node] <= nextCost)  continue;
+				cost[next.node] = nextCost;
+				trace[next.node] = current.node;
 					
-					pq.offer(next.node);
-				}
+				pq.offer(new Node(next.node, cost[next.node]));
 			}
 		}
 	}
