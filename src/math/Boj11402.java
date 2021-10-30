fbdee740d4b367812d4d585f1a3b7132a0bbd236
minuk8932 <minuk8932@naver.com>
2021-10-30 17:23:50 +0900

    feat Math(Binomial coefficient) 211030

---
+++ src/math/Boj11402.java
@@ -0,0 +1,83 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 11402번: 이항 계수 4
+ *
+ * @see https://www.acmicpc.net/problem/11402
+ *
+ */
+public class Boj11402 {
+
+    private static long[] factorial;
+
+    private static final int SIZE = 2_001;
+    private static long mod;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        mod = M;
+
+        init();
+        long result = 1L;
+
+        while (N > 0 || K > 0) {
+            long modN = N % M;
+            long modK = K % M;
+
+            if(modN < modK) {
+                result = 0;
+                break;
+            }
+
+            result = MODULATION_MUL.apply(result, MODULATION_MUL.apply(factorial[(int) modN]
+                    , MODULATION_MUL.apply(pow(factorial[(int) (modN - modK)], M - 2)
+                            , pow(factorial[(int) modK], M - 2))));
+
+            N /= M;
+            K /= M;
+        }
+
+        System.out.println(result);
+    }
+
+    /**
+     *
+     * Lucas' Theorem
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    private static long pow(long x, long y) {
+        if (y == 1) return x;
+
+        long powShift = pow(x, y >> 1L);
+
+        if(y % 2 == 0) return MODULATION_MUL.apply(powShift, powShift);
+        return MODULATION_MUL.apply(x, MODULATION_MUL.apply(powShift, powShift));
+    }
+
+    private static void init() {
+        factorial = new long[SIZE];
+        Arrays.fill(factorial, 1L);
+
+        for(int i = 2; i < SIZE; i++) {
+            factorial[i] = MODULATION_MUL.apply(factorial[i - 1], (long) i);
+        }
+    }
+
+    private static final BiFunction<Long, Long, Long> MODULATION_MUL = (x, y) -> ((x % mod) * (y % mod)) % mod;
+}
