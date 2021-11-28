b574b1c6d561d14bd4ef313d464ceda5bcd127a7
minuk8932 <minuk8932@naver.com>
2021-11-28 13:33:44 +0900

    sogang contest B

---
+++ src/sogang/B.java
@@ -0,0 +1,47 @@
+package sogang;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 서강 B번: 벼락치기
+ *
+ * @see https://www.acmicpc.net/contest/problem/725/2
+ *
+ */
+public class B {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] chapter = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            chapter[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(study(chapter));
+    }
+
+    private static int study(int[] chaps) {
+        int count = 0;
+        int time = 30;
+
+        for(int chap: chaps) {
+            int half = (chap >> 1) + chap % 2;
+            chap -= time;
+
+            if(chap < 0) {
+                count++;
+                time = Math.abs(chap);
+            }
+            else {
+                if(half <= time) count++;
+                time = 30;
+            }
+        }
+
+        return count;
+    }
+}
