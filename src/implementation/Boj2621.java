b14029345d1effb6e489afe0b398ea7eb876844f
minuk8932 <minuk8932@naver.com>
2021-12-17 00:08:37 +0900

    feat Implementation(Card game) 211217

--- src/implementation/Boj2621.java
+++ src/implementation/Boj2621.java
@@ -1,7 +1,17 @@
+package implementation;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ *
+ * @author exponential-e
+ * 백준 2621번: 카드 게임
+ *
+ * @see https://www.acmicpc.net/problem/2621
+ *
+ */
 public class Boj2621 {
 
     private static Map<Character, Integer> colorMap = new HashMap<>();
@@ -16,27 +26,11 @@ public class Boj2621 {
             cards[i] = new Card(st.nextToken().charAt(0), Integer.parseInt(st.nextToken()));
         }
 
-        /**
-         *
-         *  2. 4숫자 동일 -> 동일 + 800
-         *  3. 3숫자 / 2숫자 각 동일 -> 3장 같 * 10 + 2장 같 + 700
-         *
-         *  6. 3 숫자 동일 -> 같은 숫자 + 400
-         *  7. 2장씩 숫자 동일 -> 큰숫자 * 10 + 작은숫자 + 300
-         *  8. 2장 숫자 동일 -> 같은 숫자 + 200
-         *  9. 가장 큰 숫자 + 100
-         */
-
         System.out.println(collecting(cards));
     }
 
     private static int collecting(Card[] cards) {
-        int sum = 0;
-
-        Arrays.sort(cards, Comparator
-                .comparingInt(Card::getNumber)
-                .thenComparing(Card::getColor)
-        );
+        Arrays.sort(cards, Comparator.comparingInt(Card::getNumber));
 
         int[] colors = new int[4];
         int[] numbers = new int[10];
@@ -49,34 +43,58 @@ public class Boj2621 {
             numbers[number]++;
         }
 
+        boolean allSame = false;
+        int result = 0;
+
         for(int i = 0; i < 4; i++) {
             if(colors[i] != 5) continue;
-            return (isConsecutive(numbers) ? 900: 600) + cards[4].getNumber();
+            allSame = true;
+
+            if(isConsecutive(numbers)) result = Math.max(900 + cards[4].getNumber(), result);
+            break;
         }
 
-        if(isConsecutive(numbers)) return cards[4].getNumber() + 500;
+        int[][] count = countings(numbers);
 
-        Map<Integer, Integer> tree = new TreeMap<>();
-        for(int i = 0; i < 10; i++) {
-            tree.put(-numbers[i], i);
+        if(count[0][4] != 0) result = Math.max(count[0][4] + 800, result);
+        if(count[0][3] != 0) {
+            if(count[0][2] != 0) result = Math.max(count[0][3] * 10 + count[0][2] + 700, result);
+            else result = Math.max(count[0][3] + 400, result);
         }
 
+        if(allSame) result = Math.max(cards[4].getNumber() + 600, result);
+        if(isConsecutive(numbers)) result = Math.max(cards[4].getNumber() + 500, result);
 
+        if (count[0][2] != 0) {
+            if(count[1][2] != 0) result = Math.max(Math.max(count[1][2], count[0][2]) * 10 + Math.min(count[1][2], count[0][2]) + 300, result);
+            else result = Math.max(count[0][2] + 200, result);
+        }
 
-        return sum;
+        return Math.max(cards[4].getNumber() + 100, result);
+    }
+
+    private static int[][] countings(int[] nums) {
+        int[][] check = new int[2][5];
+
+        for (int val = 1; val < nums.length; val++) {
+            if(check[0][nums[val]] != 0) check[1][nums[val]] = val;
+            else check[0][nums[val]] = val;
+        }
+
+        return check;
     }
 
     private static boolean isConsecutive(int[] nums) {
         int count = 0;
 
-        for(int i = 0; i < nums.length; i++) {
-            if(count == 5) return true;
+        for(int i = 1; i < nums.length; i++) {
+            if (count == 5) return true;
 
             if(nums[i] != 0) count++;
             else count = 0;
         }
 
-        return false;
+        return count == 5;
     }
 
     private static void coloring() {
