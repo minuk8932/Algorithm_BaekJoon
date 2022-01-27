f03b2ce648762edc923ca421e8c6e10030985d83
minuk8932 <minuk8932@naver.com>
2022-01-27 14:22:28 +0900

    refactor StringHandle(Security algorithm) 220127

--- src/string_handle/Boj9322.java
+++ src/string_handle/Boj9322.java
@@ -15,52 +15,72 @@ import java.util.StringTokenizer;
 public class Boj9322 {
 	private static final char SPACE = ' ';
 	private static final char NEW_LINE = '\n';
-	
+
+	private static HashMap<String, Integer> publicKey1;
+	private static String[] publicKey2;
+	private static String[] pw;
+
+	private static int n;
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		
+
 		while(T-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			HashMap<String, Integer> publicKey1  = new HashMap<>();
-			String[] publicKey2  = new String[n];
-			String[] pw = new String[n];
-			
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++) {
-				publicKey1.put(st.nextToken(), i);
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++) {
-				publicKey2[i] = st.nextToken();
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < n; i++) {
-				pw[i] = st.nextToken();
-			}
-			
-			sb.append(decoding(publicKey1, publicKey2, pw)).append(NEW_LINE);
+			n = Integer.parseInt(br.readLine());
+			init();
+
+			publicKeySetter(br.readLine());
+			publicKeyRearranged(br.readLine());
+			passwordInput(br.readLine());
+
+			sb.append(decoding()).append(NEW_LINE);
 		}
-		
+
 		System.out.println(sb);
 	}
-	
-	private static StringBuilder decoding(HashMap<String, Integer> hm, String[] arr, String[] password) {
-		String[] res = new String[arr.length];
-		
-		for(int i = 0; i < arr.length; i++) {
-			int idx = hm.get(arr[i]);			// 순서 조정
-			res[idx] = password[i];
+
+	private static void passwordInput(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		for(int i = 0; i < n; i++) {
+			pw[i] = st.nextToken();
 		}
-		
+	}
+
+	private static void publicKeySetter(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		for(int i = 0; i < n; i++) {
+			publicKey1.put(st.nextToken(), i);
+		}
+	}
+
+	private static void publicKeyRearranged(String input) {
+		StringTokenizer st = new StringTokenizer(input);
+		for(int i = 0; i < n; i++) {
+			publicKey2[i] = st.nextToken();
+		}
+	}
+
+	private static void init() {
+		publicKey1  = new HashMap<>();
+		publicKey2  = new String[n];
+		pw = new String[n];
+	}
+
+	private static StringBuilder decoding() {
+		String[] res = new String[publicKey2.length];
+
+		for(int i = 0; i < publicKey2.length; i++) {
+			int idx = publicKey1.get(publicKey2[i]);
+			res[idx] = pw[i];
+		}
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < res.length; i++) {		// 암호 해독
+		for(int i = 0; i < res.length; i++) {
 			sb.append(res[i]).append(SPACE);
 		}
-		
+
 		return sb;
 	}
 }
