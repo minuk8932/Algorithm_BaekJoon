3bb248990487ff6c192e4557960f3b83a168cbba
minuk8932 <minuk8932@naver.com>
2022-01-31 12:34:11 +0900

    refactor Node added another

--- src/common/Node.java
+++ src/common/Node.java
@@ -2,11 +2,22 @@ package common;
 
 public class Node {
     private int node;
+    private int another;
     private int cost;
 
     public Node(int node, int cost) {
         this.node = node;
         this.cost = cost;
+    }
+
+    public Node(int node, int another, int cost) {
+        this.node = node;
+        this.another = another;
+        this.cost = cost;
+    }
+
+    public int getAnother() {
+        return another;
     }
 
     public int getNode() {
