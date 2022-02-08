3923612a0bc0ccbcde667054fab34455b0da1d43
minuk8932 <minuk8932@naver.com>
2022-02-08 15:59:41 +0900

    feat BFS(Algorithm lecture - BFS1) 220208

---
+++ src/breadth_first_search/Boj24444.java
@@ -0,0 +1,74 @@
+package breadth_first_search;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 24444번: 알고리즘 수업 - 너비 우선 탐색 1
+ *
+ * @see https://www.acmicpc.net/problem/24444
+ *
+ */
+public class Boj24444 {
+
+    private static final String NEW_LINE = "\n";
+    private static List<Integer>[] graph;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken()) - 1;
+
+        graph = new ArrayList[N];
+        for(int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for(int i = 0; i < N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        System.out.println(bfs(R));
+    }
+
+    private static String bfs(int start) {
+        int[] visit = new int[graph.length];
+        int count = 1;
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visit[start] = count++;
+
+        while(!q.isEmpty()) {
+            int current = q.poll();
+
+            for(int next: graph[current]) {
+                if(visit[next] != 0) continue;
+                visit[next] = count++;
+
+                q.offer(next);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int v: visit) {
+            sb.append(v).append(NEW_LINE);
+        }
+
+        return sb.toString();
+    }
+}
