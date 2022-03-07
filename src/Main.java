f4cdca1db3e5de8d6ed40e885b1d548efe071fcb
minuk8932 <minuk8932@naver.com>
2022-03-07 15:58:04 +0900

    refactor Pair class added third

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,45 @@
+import common.Pair;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static final String SPACE = " ";
+
+    private static HashMap<Integer, Integer> map = new HashMap<>();
+    private static HashMap<Integer, Integer> index = new HashMap<>();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st.nextToken();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            map.merge(value, 1, Integer::sum);
+
+            if(index.containsKey(value)) continue;
+            index.put(value, i);
+        }
+
+        System.out.println(getSequence());
+    }
+
+    private static StringBuilder getSequence() {
+        PriorityQueue<Pair<Integer>> pq = new PriorityQueue<>(Comparator
+                .comparingInt(Pair<Integer>::getFirst)
+                .thenComparingInt(Pair::getSecond));
+
+        for(int key: map.keySet()){
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        return sb;
     }
 }
--- src/common/Pair.java
+++ src/common/Pair.java
@@ -4,16 +4,24 @@ public class Pair<T> {
     private final T first;
     private final T second;
 
+    private final T third;
+
     public static class Builder<T> {
         // must
         private final T first;
         private final T second;
 
         // opt
+        private T third;
 
         public Builder(T first, T second) {
             this.first = first;
             this.second = second;
+        }
+
+        public Builder another(T value) {
+            third = value;
+            return this;
         }
 
         public Pair build() {
@@ -24,6 +32,7 @@ public class Pair<T> {
     private Pair(Builder<T> builder) {
         first = builder.first;
         second = builder.second;
+        third = builder.third;
     }
 
     public T getFirst() {
@@ -32,5 +41,9 @@ public class Pair<T> {
 
     public T getSecond() {
         return second;
+    }
+
+    public T getThird() {
+        return third;
     }
 }
