f344ad34ce017e9963d808c6b13ca541a64485a0
minuk8932 <minuk8932@naver.com>
2022-03-07 16:05:24 +0900

    refactor Heap(Frequency sort) 220307

--- src/heap/Boj2910.java
+++ src/heap/Boj2910.java
@@ -1,13 +1,12 @@
 package heap;
+import common.Pair;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
- * 
+ *
  * 	@author minchoba
  *	백준 2910번: 빈도 정렬
  *
@@ -16,79 +15,52 @@ import java.util.StringTokenizer;
  */
 public class Boj2910 {
 	private static final String SPACE = " ";
-	
-	private static class Info implements Comparable<Info>{
-		int index;
-		int value;
-		int count;
-		
-		public Info(int index, int value, int count) {
-			this.index = index;
-			this.value = value;
-			this.count = count;
-		}
 
-		@Override
-		public int compareTo(Info i) {
-			if(this.count > i.count) {
-				return -1;
-			}
-			else if(this.count < i.count) {
-				return 1;
-			}
-			else {
-				if(this.index < i.index) return -1;
-				else if(this.index > i.index) return 1;
-				else return 0;
-			}
-		}
-	}
-	
+	private static HashMap<Integer, Integer> map = new HashMap<>();
+	private static HashMap<Integer, Integer> index = new HashMap<>();
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		
-		HashMap<Integer, Integer> hm = new HashMap<>();
-		HashMap<Integer, Integer> arr = new HashMap<>();
-		HashSet<Integer> set = new HashSet<>();
-		
+		st.nextToken();
+
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			int value = Integer.parseInt(st.nextToken());
-			
-			set.add(value);			// 숫자 종류
-			
-			if(!arr.containsKey(value)) arr.put(value, i);		// 숫자와 등장 순서
-			else arr.put(value, Math.min(arr.get(value), i));
-			
-			if(!hm.containsKey(value)) hm.put(value, 1);		// 숫자와 등장 빈도
-			else hm.put(value, hm.get(value) + 1);
+
+			map.merge(value, 1, Integer::sum);
+
+			if(index.containsKey(value)) continue;
+			index.put(value, i);
 		}
-		
-		System.out.println(getSequence(N, set, arr, hm));
+
+		System.out.println(getSequence());
 	}
-	
-	private static StringBuilder getSequence(int n, HashSet<Integer> set, HashMap<Integer, Integer> arr, HashMap<Integer, Integer> hm) {
-		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Info> pq = new PriorityQueue<>();
-		
-		for(int val: set) {
-			int idx = arr.get(val);
-			int cnt = hm.get(val);
-			
-			pq.offer(new Info(idx, val, cnt));			// 우선순위 큐에서 조건에 따라 정렬
+
+	private static StringBuilder getSequence() {
+		PriorityQueue<Pair<Integer>> pq = new PriorityQueue<>(Comparator
+				.comparingInt(Pair<Integer>::getSecond)
+				.thenComparingInt(Pair::getThird));
+
+		for(int key: map.keySet()){
+			pq.offer(new Pair.Builder(key, -map.get(key))
+					.third(index.get(key))
+					.build());
 		}
-		
+
+		StringBuilder sb = new StringBuilder();
+
 		while(!pq.isEmpty()) {
-			Info current = pq.poll();
-			
-			while(current.count-- > 0) {
-				sb.append(current.value).append(SPACE);
+			Pair<Integer> current = pq.poll();
+			int loop = -current.getSecond();
+
+			while(loop-- > 0) {
+				sb.append(current.getFirst()).append(SPACE);
 			}
 		}
-		
+
 		return sb;
 	}
 }
+
