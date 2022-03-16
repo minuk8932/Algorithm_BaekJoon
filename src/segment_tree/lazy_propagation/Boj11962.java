8b2a4e34fd4366b5304ef425d99aad5d7ab8a8ec
minuk8932 <minuk8932@naver.com>
2022-03-16 16:31:29 +0900

    refactor SegmentTree(Counting haybales)220316

--- src/segment_tree/lazy_propagation/Boj11962.java
+++ src/segment_tree/lazy_propagation/Boj11962.java
@@ -15,13 +15,13 @@ import java.util.StringTokenizer;
 public class Boj11962 {
 
     private static final long INF = 1_000_000_000_000L;
-    private static final int SIZE = 1 << 21;
 
+    private static int SIZE;
     private static int S;
 
-    private static long[] tree = new long[SIZE];
-    private static long[] lazy = new long[SIZE];
-    private static long[] min = new long[SIZE];
+    private static long[] tree;
+    private static long[] lazy;
+    private static long[] min;
 
     private static final String SUM = "S";
     private static final String MIN = "M";
@@ -34,13 +34,16 @@ public class Boj11962 {
         int N = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
 
-        init();
+        init(N);
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {
-            sectionUpdate(i, i, Integer.parseInt(st.nextToken()), 1, 0, S - 1);
+        for(int i = 0; i < N; i++) {
+            int index = S + i;
+            tree[index] = Integer.parseInt(st.nextToken());
+            min[index] = tree[index];
         }
 
+        accumulation();
         StringBuilder sb = new StringBuilder();
 
         while(Q-- > 0) {
@@ -51,16 +54,16 @@ public class Boj11962 {
 
             switch (cmd) {
                 case SUM:
-                    sb.append(sectionSum(A, B, 1, 0, S - 1)).append(NEW_LINE);
+                    sb.append(sectionSum(A, B, 1, 1, S)).append(NEW_LINE);
                     break;
 
                 case MIN:
-                    sb.append(sectionMin(A, B, 1, 0, S - 1)).append(NEW_LINE);
+                    sb.append(sectionMin(A, B, 1, 1, S)).append(NEW_LINE);
                     break;
 
                 case PUT:
                     long C = Long.parseLong(st.nextToken());
-                    sectionUpdate(A, B, C, 1, 0, S - 1);
+                    sectionUpdate(A, B, C, 1, 1, S);
                     break;
             }
         }
@@ -68,57 +71,52 @@ public class Boj11962 {
         System.out.println(sb.toString());
     }
 
-    private static long sectionMin(int inStart, int inEnd, int node, int pStart, int pEnd) {
-        propagation(node, pStart, pEnd);
+    private static long sectionMin(int start, int end, int node, int currentStart, int currentEnd) {
+        propagation(node, currentStart, currentEnd);
 
-        if(inEnd < pStart || pEnd < inStart) return INF;
-        if(inStart <= pStart && pEnd <= inEnd) return min[node];
+        if(end < currentStart || currentEnd < start) return INF;
+        if(start <= currentStart && currentEnd <= end) return min[node];
 
-        int mid = (pStart + pEnd) >> 1;
+        int mid = (currentStart + currentEnd) >> 1;
         int son = node << 1;
-        return Math.min(sectionMin(inStart, inEnd, son, pStart, mid),
-            sectionMin(inStart, inEnd, son + 1, mid + 1, pEnd));
+        return Math.min(sectionMin(start, end, son, currentStart, mid),
+            sectionMin(start, end, son + 1, mid + 1, currentEnd));
     }
 
-    private static long sectionSum(int inStart, int inEnd, int node, int pStart, int pEnd) {
-        propagation(node, pStart, pEnd);
+    private static long sectionSum(int start, int end, int node, int currentStart, int currentEnd) {
+        propagation(node, currentStart, currentEnd);
 
-        if(inEnd < pStart || pEnd < inStart) return 0L;
-        if(inStart <= pStart && pEnd <= inEnd) return tree[node];
+        if(end < currentStart || currentEnd < start) return 0L;
+        if(start <= currentStart && currentEnd <= end) return tree[node];
 
-        int mid = (pStart + pEnd) >> 1;
+        int mid = (currentStart + currentEnd) >> 1;
         int son = node << 1;
-        return sectionSum(inStart, inEnd, son, pStart, mid)
-            + sectionSum(inStart, inEnd, son + 1, mid + 1, pEnd);
+        return sectionSum(start, end, son, currentStart, mid)
+            + sectionSum(start, end, son + 1, mid + 1, currentEnd);
     }
 
-    private static void sectionUpdate(int inStart, int inEnd, long value, int node, int pStart, int pEnd){
-        propagation(node, pStart, pEnd);
+    private static void sectionUpdate(int start, int end, long value, int node
+        , int currentStart, int currentEnd){
+        propagation(node, currentStart, currentEnd);
 
-        if(inEnd < pStart || pEnd < inStart) return;
-        if(inStart <= pStart && pEnd <= inEnd){
-            if (pStart != pEnd) {
-                lazy[node * 2] += value;
-                lazy[node * 2 + 1] += value;
-            }
-
-            min[node] += value;
-            tree[node] += value * (pEnd - pStart + 1);
-
+        if(end < currentStart || currentEnd < start) return;
+        if(start <= currentStart && currentEnd <= end){
+            lazy[node] += value;
+            propagation(node, currentStart, currentEnd);
             return;
         }
 
         int son = node << 1;
-        int mid = (pStart + pEnd) >> 1;
+        int mid = (currentStart + currentEnd) >> 1;
 
-        sectionUpdate(inStart, inEnd, value, son, pStart, mid);
-        sectionUpdate(inStart, inEnd, value, son + 1, mid + 1, pEnd);
+        sectionUpdate(start, end, value, son, currentStart, mid);
+        sectionUpdate(start, end, value, son + 1, mid + 1, currentEnd);
 
         tree[node] = tree[son] + tree[son + 1];
         min[node] = Math.min(min[son], min[son + 1]);
     }
 
-    private static void propagation(int node, int pStart, int pEnd) {
+    private static void propagation(int node, int currentStart, int currentEnd) {
         if(lazy[node] == 0) return;
 
         if(node < S) {
@@ -127,12 +125,30 @@ public class Boj11962 {
             lazy[son + 1] += lazy[node];
         }
 
-        tree[node] += lazy[node] * (pEnd - pStart + 1);
+        tree[node] += lazy[node] * (currentEnd - currentStart + 1);
         min[node] += lazy[node];
         lazy[node] = 0;
     }
 
-    private static void init() {
-        S = SIZE >> 1;
+    private static void accumulation() {
+        for(int i = S - 1; i > 0; i--) {
+            int son = i << 1;
+            tree[i] = tree[son] + tree[son + 1];
+            min[i] = Math.min(min[son], min[son + 1]);
+        }
+    }
+
+    private static void init(int n) {
+        SIZE = 1;
+        while(SIZE <= n) {
+            SIZE <<= 1;
+        }
+
+        S = SIZE;
+        SIZE <<= 1;
+
+        tree = new long[SIZE];
+        min = new long[SIZE];
+        lazy = new long[SIZE];
     }
 }
