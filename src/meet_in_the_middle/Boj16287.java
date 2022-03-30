40b05ee7288b71051b73ef625a4ff563879ac4f9
minuk8932 <minuk8932@naver.com>
2022-03-30 10:01:56 +0900

    feat MeetInTheMiddle(Parcel) 220330

--- src/meet_in_the_middle/Boj16287.java
+++ src/meet_in_the_middle/Boj16287.java
@@ -1,9 +1,18 @@
+package meet_in_the_middle;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
+/**
+ *
+ * @author exponential-e
+ * 백준 16287번: Parcel
+ *
+ * @see https://www.acmicpc.net/problem/16287
+ *
+ */
 public class Boj16287 {
 
     private static final int INF = 1_000_001;
@@ -23,6 +32,17 @@ public class Boj16287 {
         System.out.println(pairing(a, w) ? "YES": "NO");
     }
 
+    /**
+     *
+     * Pairing
+     *
+     * line 52 ~ 55: make sum, find another sum
+     * line 57 ~ 58: setting max index in pair array, and check is duplicated sum
+     *
+     * @param a
+     * @param weight
+     * @return
+     */
     private static boolean pairing(int[] a, int weight) {
         int[] pairs = new int[weight + 1];
         Arrays.fill(pairs, INF);
