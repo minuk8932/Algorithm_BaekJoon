f21f07659fc5836d7b02c44ec83be6b99bd30c81
minuk8932 <minuk8932@naver.com>
2022-04-07 11:51:29 +0900

    feat DP(Pipe moving 2) 220407

--- src/Boj9025.java
+++ src/Boj9025.java
@@ -6,11 +6,16 @@ import java.util.*;
 
 public class Boj9025 {
 
-    private static final int INF = 1_000_000_000;
     private static final String NEW_LINE = "\n";
+    private static final int INF = 1_000_000_000;
 
-    private static List<Node>[] path;
+    private static List<Node<Integer, Integer>>[] path;
+
+    private static int[][] input;
     private static int[] dist;
+    private static int[] parent;
+
+    private static PriorityQueue<Node<Integer, Integer>> pq;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,82 +31,110 @@ public class Boj9025 {
 
             init(n);
 
-            int[][] inputs = new int[n][n];
-            for(int i = 0; i < n; i++) {
-                Arrays.fill(inputs[i], INF);
-            }
-
             while(m-- > 0) {
                 st = new StringTokenizer(br.readLine());
                 int from = Integer.parseInt(st.nextToken()) - 1;
                 int to = Integer.parseInt(st.nextToken()) - 1;
                 int cost = Integer.parseInt(st.nextToken());
 
-                inputs[from][to] = Math.min(cost, inputs[from][to]);
-                inputs[to][from] = inputs[from][to];
+                input[from][to] = Math.min(input[from][to], cost);
+                input[to][from] = input[from][to];
             }
 
-            for(int i = 0; i < n; i++) {
-                for(int j = i + 1; j < n; j++) {
-                    path[i].add(new Node.Builder(j).cost(inputs[i][j]).build());
-                    path[j].add(new Node.Builder(i).cost(inputs[j][i]).build());
-                }
-            }
+            offline();
+            mst();
 
-            dijkstra(s);
-            sb.append(search(t)).append(NEW_LINE);
+            sb.append(dijkstra(s, t)).append(NEW_LINE);
         }
 
         System.out.println(sb.toString());
     }
 
-    private static int search(int snk) {
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node.Builder<Integer, Integer>(snk).cost(dist[snk]).build());
-
-        int min = INF;
-
-        while(!q.isEmpty()) {
-            Node<Integer, Integer> current = q.poll();
-
-            for (Node<Integer, Integer> next: path[current.getNode()]) {
-                if(dist[next.getNode()] != dist[current.getNode()] - current.getCost()) continue;
-                min = Math.min(min, -current.getCost());
-
-                q.offer(new Node.Builder(next.getNode()).cost(dist[next.getNode()]).build());
+    private static void offline() {
+        for(int from = 0; from < input.length; from++) {
+            for(int to = from + 1; to < input.length; to++) {
+                if(input[from][to] == INF) continue;
+                pq.offer(new Node.Builder(from).another(to).cost(-input[from][to]).build());
             }
         }
-
-        return min;
     }
 
-    private static void dijkstra(int src) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(Node<Integer, Integer>::getCost));
-        pq.offer(new Node.Builder(src).cost(0).build());
-        Arrays.fill(dist, INF);
+    private static int dijkstra(int src, int snk) {
+        int answer = INF;
 
+        pq.offer(new Node.Builder(src).cost(0).build());
         dist[src] = 0;
 
         while(!pq.isEmpty()) {
             Node<Integer, Integer> current = pq.poll();
-            if(dist[current.getNode()] < current.getCost()) continue;
 
             for(Node<Integer, Integer> next: path[current.getNode()]) {
-                if(dist[next.getNode()] <= dist[current.getNode()] + next.getCost()) continue;
-                dist[next.getNode()] = dist[current.getNode()] + next.getCost();
+                if(dist[next.getNode()] <= dist[current.getNode()] + current.getCost()) continue;
+                dist[next.getNode()] = dist[current.getNode()] + current.getCost();
+
+                answer = Math.min(answer, next.getCost());
+                if(next.getNode() == snk) return answer;
 
                 pq.offer(new Node.Builder(next.getNode()).cost(dist[next.getNode()]).build());
             }
         }
+
+        return answer;
+    }
+
+    private static void mst() {
+        while(!pq.isEmpty()) {
+            Node<Integer, Integer> current = pq.poll();
+
+            if(merged(current.getNode(), current.getAnother())) continue;
+            path[current.getNode()].add(
+                new Node.Builder(current.getAnother())
+                    .cost(-current.getCost())
+                    .build());
+
+            path[current.getAnother()].add(
+                new Node.Builder(current.getNode())
+                    .cost(-current.getCost())
+                    .build());
+        }
+    }
+
+    private static int find(int x) {
+        if(parent[x] < 0) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static boolean merged(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return true;
+
+        if(parent[x] < parent[y]) {
+            parent[x] += parent[y];
+            parent[y] = x;
+        }
+        else {
+            parent[y] += parent[x];
+            parent[x] = y;
+        }
+
+        return false;
     }
 
     private static void init(int n) {
-        dist = new int[n];
         path = new ArrayList[n];
+        parent = new int[n];
+        dist = new int[n];
+        input = new int[n][n];
+
+        pq = new PriorityQueue<>(Comparator.comparingInt(Node::getCost));
 
         for(int i = 0; i < n; i++){
-            dist[i] = -1;
             path[i] = new ArrayList<>();
+            parent[i] = -1;
+            dist[i] = INF;
+            Arrays.fill(input[i], INF);
         }
     }
 }
---
+++ src/dynamic_programming/Boj17069.java
@@ -0,0 +1,126 @@
+package dynamic_programming;
+
+import common.Point;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 17069번: 파이프 옮기기 2
+ *
+ * @see https://www.acmicpc.net/problem/17069
+ *
+ */
+public class Boj17069 {
+
+    private static long[][][][] dp;
+    private static int[][] map;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        map = new int[N + 1][N + 1];
+        for(int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 1; j <= N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp = new long[N + 1][N + 1][N + 1][N + 1];
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp.length; j++) {
+                for(int k = 0; k < dp.length; k++) {
+                    Arrays.fill(dp[i][j][k], -1L);
+                }
+            }
+        }
+
+        long left = recursion(
+            new Point.Builder(N, N).build()
+            , new Point.Builder(N, N - 1).build()
+            , 0
+        );
+
+        long up = recursion(
+            new Point.Builder(N, N).build()
+            , new Point.Builder(N - 1, N).build()
+            , 1
+        );
+
+        long diagonal = recursion(
+            new Point.Builder(N, N).build()
+            , new Point.Builder(N - 1, N - 1).build()
+            , 2
+        );
+
+        System.out.println(left + up + diagonal);
+    }
+
+    private static long recursion(Point<Integer, Integer> post, Point<Integer, Integer> prev, int type) {
+        if(prev.getRow() <= 0 || prev.getCol() <= 0) return 0;
+
+        if(type == 2 && (map[post.getRow()][post.getCol() - 1] == 1
+            || map[post.getRow() - 1][post.getCol()] == 1)) return 0;
+
+        if(map[prev.getRow()][prev.getCol()] == 1
+            || map[post.getRow()][post.getCol()] == 1) return 0;
+
+        if(prev.getRow() == 1 && prev.getCol() == 1
+            && post.getRow() == 1 && post.getCol() == 2) return 1;
+
+        if(dp[post.getRow()][post.getCol()][prev.getRow()][prev.getCol()] != -1)
+            return dp[post.getRow()][post.getCol()][prev.getRow()][prev.getCol()];
+
+        long answer = recursion(
+            new Point.Builder(prev.getRow(), prev.getCol()).build()
+            , new Point.Builder(prev.getRow() - 1, prev.getCol() - 1).build()
+            , 2
+        );
+
+        long nextLeft;
+        long nextUp;
+
+        if(type == 0){
+            nextLeft = recursion(
+                new Point.Builder(prev.getRow(), prev.getCol()).build()
+                , new Point.Builder(prev.getRow(), prev.getCol() - 1).build()
+                , 0
+            );
+
+            answer += nextLeft;
+        }
+        else if(type == 1){
+
+            nextUp = recursion(
+                new Point.Builder(prev.getRow(), prev.getCol()).build()
+                , new Point.Builder(prev.getRow() - 1, prev.getCol()).build()
+                , 1
+            );
+
+            answer += nextUp;
+        }
+        else {
+            nextLeft = recursion(
+                new Point.Builder(prev.getRow(), prev.getCol()).build()
+                , new Point.Builder(prev.getRow(), prev.getCol() - 1).build()
+                , 0
+            );
+
+            nextUp = recursion(
+                new Point.Builder(prev.getRow(), prev.getCol()).build()
+                , new Point.Builder(prev.getRow() - 1, prev.getCol()).build()
+                , 1
+            );
+
+            answer += nextLeft + nextUp;
+        }
+
+        return dp[post.getRow()][post.getCol()][prev.getRow()][prev.getCol()] = answer;
+    }
+}
