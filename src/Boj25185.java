32cabd324e6351627d62a880f274f90dddc9030b
minuk8932 <minuk8932@naver.com>
2022-05-21 16:54:17 +0900

    feat Common class Card

---
+++ src/Boj25185.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj25185 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+        }
+    }
+}
---
+++ src/common/Card.java
@@ -0,0 +1,34 @@
+package common;
+
+public class Card<T, E> {
+    private final T number;
+    private final E word;
+
+    private Card(Card.Builder<T, E> builder) {
+        number = builder.number;
+        word = builder.word;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public E getWord() {
+        return word;
+    }
+
+    public static class Builder<T, E> {
+        // must
+        private final T number;
+        private final E word;
+
+        public Builder(T number, E word) {
+            this.number = number;
+            this.word = word;
+        }
+
+        public Card build() {
+            return new Card(this);
+        }
+    }
+}
