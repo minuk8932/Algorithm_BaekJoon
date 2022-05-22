0218d45a3b33303a961c2e721f428d1987b00bd5
minuk8932 <minuk8932@naver.com>
2022-05-22 13:06:42 +0900

    refactor add Equals&HashCode in Point class

--- src/common/Point.java
+++ src/common/Point.java
@@ -1,32 +1,11 @@
 package common;
 
+import java.util.Objects;
+
 public class Point<T, E> {
     private final T row;
     private final T col;
     private final E cost;
-
-    public static class Builder<T, E> {
-        // must
-        private final T row;
-        private final T col;
-
-        // opt
-        private E cost;
-
-        public Builder (T row, T col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        public Builder cost(E value) {
-            cost = value;
-            return this;
-        }
-
-        public Point build() {
-            return new Point(this);
-        }
-    }
 
     private Point(Builder<T, E> builder) {
         row = builder.row;
@@ -50,5 +29,46 @@ public class Point<T, E> {
 
     public E getCost() {
         return cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Point)) {
+            return false;
+        }
+        Point<?, ?> point = (Point<?, ?>) o;
+        return row.equals(point.row) && col.equals(point.col) && Objects.equals(cost,
+            point.cost);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col, cost);
+    }
+
+    public static class Builder<T, E> {
+        // must
+        private final T row;
+        private final T col;
+
+        // opt
+        private E cost;
+
+        public Builder (T row, T col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public Builder cost(E value) {
+            cost = value;
+            return this;
+        }
+
+        public Point build() {
+            return new Point(this);
+        }
     }
 }
