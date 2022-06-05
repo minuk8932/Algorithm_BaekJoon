9ef5e5176f20c6e3145bb3c31da965c69f10e284
minuk8932 <minuk8932@naver.com>
2022-06-05 13:20:02 +0900

    feat DisjointSet(Coogie's graduation trip) 220605

---
+++ src/disjoint_set/Boj25172.java
@@ -0,0 +1,114 @@
+package disjoint_set;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author exponential-e
+ * 백준 25172번: 꼼꼼한 쿠기의 졸업여행
+ *
+ * @see https://www.acmicpc.net/problem/25172
+ *
+ */
+public class Boj25172 {
+
+    private static final String C = "CONNECT\n";
+    private static final String D = "DISCONNECT\n";
+
+    private static ArrayList<ArrayList<Integer>> connections = new ArrayList<>();
+    private static int[] parent;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N];
+        Arrays.fill(parent, -1);
+
+        for(int i = 0; i < N; i++) {
+            connections.add(new ArrayList<>());
+        }
+
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken()) - 1;
+            int node2 = Integer.parseInt(st.nextToken()) - 1;
+
+            connections.get(node1).add(node2);
+            connections.get(node2).add(node1);
+        }
+
+        ArrayDeque<Integer> query = new ArrayDeque<>();
+        while(N-- > 0) {
+            query.push(Integer.parseInt(br.readLine()) - 1);
+        }
+
+        System.out.print(travel(query));
+    }
+
+    /**
+     *
+     * Traveling
+     *
+     * line 71 ~ 84: offline query process, revoke sequence
+     *
+     * @param query
+     * @return
+     */
+    private static String travel(ArrayDeque<Integer> query) {
+        long count = 0;
+        boolean[] visit = new boolean[query.size()];
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        stack.push(D);
+        while(!query.isEmpty()) {
+            int current = query.pop();
+            count++;
+
+            visit[current] = true;
+
+            for(int adj: connections.get(current)) {
+                if(!visit[adj]) continue;
+                merge(current, adj);
+            }
+
+            stack.push(count == -parent[find(current)] ? C: D);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.toString();
+    }
+
+    private static int find(int x) {
+        if (parent[x] < 0) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+
+        if (parent[x] < parent[y]) {
+            parent[x] += parent[y];
+            parent[y] = x;
+        }
+        else {
+            parent[y] += parent[x];
+            parent[x] = y;
+        }
+    }
+}
