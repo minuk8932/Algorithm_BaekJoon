6d9485a371950d1832e4cf6983a9bf8024807cd6
minuk8932 <minuk8932@naver.com>
2022-09-12 14:42:06 +0900

    feat BFS(SR departmentstore tour) 220912

--- src/breadth_first_search/Boj25307.java
+++ src/breadth_first_search/Boj25307.java
@@ -1,12 +1,9 @@
 package breadth_first_search;
 
 import common.Point;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.util.function.Predicate;
 
 /**
  *
@@ -18,66 +15,76 @@ import java.util.StringTokenizer;
  */
 public class Boj25307 {
 
+    private static final LinkedList<Point<Integer, Integer>> MANNEQUINS = new LinkedList<>();
+    private static final int PILLAR = 1;
+    private static final int SR = 4;
+    private static final int CHAIR = 2;
+    private static final int MANNEQUIN = 3;
     private static final int[][] DIRECTIONS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     private static final int ROW = 0, COL = 1;
-    private static final int MANNEQUIN = 3;
-    private static final int PILLAR = 1;
-    private static final int CHAIR = 2;
-    private static final int START = 4;
 
-    private static int[][] departmentStore;
-    private static int N, M;
-    private static ArrayList<Point<Integer, Integer>> mannequins = new ArrayList<>();
     private static Point<Integer, Integer> start;
+    private static int[][] departmentStore;
+
+    private static int N, M, K;
+    private static final Predicate<Point<Integer, Integer>> OUT_OF_RANGE = point ->
+        point.getRow() < 0 || point.getCol() < 0 || point.getRow() >= N || point.getCol() >= M;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         departmentStore = new int[N][M];
-
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
             for(int j = 0; j < M; j++) {
                 departmentStore[i][j] = Integer.parseInt(st.nextToken());
-
+                if(departmentStore[i][j] == SR)
+                    start = new Point.Builder<Integer, Integer>(i, j).build();
                 if(departmentStore[i][j] == MANNEQUIN)
-                    mannequins.add(new Point.Builder(i, j).build());
-                if(departmentStore[i][j] == START)
-                    start = new Point.Builder(i, j).build();
+                    MANNEQUINS.add(new Point.Builder<Integer, Integer>(i, j).build());
             }
         }
 
-        mannequinFills(K);
-        System.out.println(findChair());
+        mannequinFloodFill();
+
+//        for(int i = 0; i < N; i++) {
+//            for(int j = 0; j < M; j++) {
+//                System.out.print(departmentStore[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+
+        System.out.println(bfs());
     }
 
-    private static int findChair() {
+    private static int bfs() {
+        Queue<Point<Integer, Integer>> q = new ArrayDeque<>();
         int[][] visit = new int[N][M];
 
-        Queue<Point<Integer, Integer>> q = new ArrayDeque<>();
         q.offer(start);
-
         visit[start.getRow()][start.getCol()] = 1;
 
         while(!q.isEmpty()) {
             Point<Integer, Integer> current = q.poll();
 
-            for(final int[] DIRECTION: DIRECTIONS) {
-                int nextRow = current.getRow() + DIRECTION[ROW];
-                int nextCol = current.getCol() + DIRECTION[COL];
+            for (final int[] DIRECTION: DIRECTIONS) {
+                Point<Integer, Integer> next = new Point.Builder<Integer, Integer>
+                    (current.getRow() + DIRECTION[ROW],
+                        current.getCol() + DIRECTION[COL])
+                    .build();
 
-                Point<Integer, Integer> next = new Point.Builder(nextRow, nextCol).build();
-
-                if(OUT_OF_RANGE(next)) continue;
-                if(departmentStore[next.getRow()][next.getCol()] == MANNEQUIN) continue;
+                if(OUT_OF_RANGE.test(next)) continue;
                 if(departmentStore[next.getRow()][next.getCol()] == PILLAR) continue;
+                if(departmentStore[next.getRow()][next.getCol()] == MANNEQUIN) continue;
                 if(visit[next.getRow()][next.getCol()] != 0) continue;
-                visit[next.getRow()][next.getCol()] = visit[current.getRow()][current.getCol()] + 1;
+                visit[next.getRow()][next.getCol()] =
+                    visit[current.getRow()][current.getCol()] + 1;
 
                 if(departmentStore[next.getRow()][next.getCol()] == CHAIR)
                     return visit[next.getRow()][next.getCol()] - 1;
@@ -86,33 +93,45 @@ public class Boj25307 {
             }
         }
 
-
         return -1;
     }
 
-    private static void mannequinFills(int effectiveRange) {
-        for(int row = 0; row < N; row++) {
-            for(int col = 0; col < M; col++) {
-                if(departmentStore[row][col] == MANNEQUIN) continue;
-                Point current = new Point.Builder(row, col).build();
+    private static void mannequinFloodFill() {
+        Queue<Point<Integer, Integer>> q = new ArrayDeque<>();
+        int[][] visit = new int[N][M];
 
-                for(Point<Integer, Integer> mannequin: mannequins) {
-                    if(manhattanDistance(mannequin, current) > effectiveRange) continue;
+        while(!MANNEQUINS.isEmpty()) {
+            Point<Integer, Integer> start = MANNEQUINS.remove();
+            q.offer(start);
 
-                    departmentStore[row][col] = MANNEQUIN;
-                    break;
+            visit[start.getRow()][start.getCol()] = 1;
+        }
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            while(size-- > 0) {
+                Point<Integer, Integer> current = q.poll();
+                if(visit[current.getRow()][current.getCol()] == K + 1) continue;
+
+                for(final int[] DIRECTION: DIRECTIONS) {
+                    Point<Integer, Integer> next = new Point.Builder<Integer, Integer>
+                            (current.getRow() + DIRECTION[ROW],
+                                current.getCol() + DIRECTION[COL])
+                            .build();
+
+                    if(OUT_OF_RANGE.test(next)) continue;
+                    if(departmentStore[next.getRow()][next.getCol()] == PILLAR) continue;
+                    if(departmentStore[next.getRow()][next.getCol()] == SR) continue;
+                    if(visit[next.getRow()][next.getCol()] != 0) continue;
+                    visit[next.getRow()][next.getCol()] =
+                        visit[current.getRow()][current.getCol()] + 1;
+
+                    departmentStore[next.getRow()][next.getCol()] = MANNEQUIN;
+
+                    q.offer(next);
                 }
             }
         }
-    }
-
-    private static int manhattanDistance(Point<Integer, Integer> mannequin
-        , Point<Integer, Integer> current) {
-        return Math.abs(mannequin.getRow() - current.getRow())
-            + Math.abs(mannequin.getCol() - current.getCol());
-    }
-
-    private static boolean OUT_OF_RANGE(Point<Integer, Integer> p) {
-        return p.getRow() < 0 || p.getCol() < 0 || p.getRow() >= N || p.getCol() >= M;
     }
 }
