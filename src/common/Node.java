13f22cf19c83864214a8dbef4f36b667f0a60bbf
minuk8932 <minuk8932@naver.com>
2022-10-02 14:27:32 +0900

    add Node(weight property)

--- src/common/Node.java
+++ src/common/Node.java
@@ -4,11 +4,13 @@ public class Node<T, E> {
     private final T node;
 
     private final E cost;
+    private final E weight;
     private final T another;
 
     private Node(Builder<T, E> builder) {
         node = builder.node;
         cost = builder.cost;
+        weight = builder.weight;
         another = builder.another;
     }
 
@@ -22,12 +24,15 @@ public class Node<T, E> {
 
     public E getCost() { return cost; }
 
+    public E getWeight() { return weight; }
+
     public static class Builder<T, E> {
         //must
         private final T node;
         // opt
         private T another;
         private E cost;
+        private E weight;
 
         public Builder(T node) {
             this.node = node;
@@ -35,6 +40,11 @@ public class Node<T, E> {
 
         public Builder<T, E> cost(E value) {
             cost = value;
+            return this;
+        }
+
+        public Builder<T, E> weight(E value) {
+            weight = value;
             return this;
         }
 
